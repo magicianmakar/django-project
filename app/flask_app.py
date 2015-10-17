a4d6e4c43f7d0a4468a8fa2de17bf495e9d0ea2d
Ahmed Saidi <justroftest@gmail.com>
2015-10-17 17:24:18 +0000

    Fix

--- app/flask_app.py
+++ app/flask_app.py
@@ -16,7 +16,7 @@ class WebsiteHeader(db.Model):
     header = db.Column(db.String(256))
     text = db.Column(db.String(512))
 
-    website_link_id = db.Column(db.Integer, db.ForeignKey('website_link.id'))
+    website_link_id = db.Column(db.Integer, db.ForeignKey('website_link.id', ondelete='cascade'))
     website_link = db.relationship('WebsiteLink', backref=db.backref('headers', lazy='dynamic'))
 
     def __init__(self, header, text, link):
@@ -32,7 +32,7 @@ class WebsiteImage(db.Model):
     src = db.Column(db.String(256))
     alt = db.Column(db.String(300))
 
-    website_link_id = db.Column(db.Integer, db.ForeignKey('website_link.id'))
+    website_link_id = db.Column(db.Integer, db.ForeignKey('website_link.id', ondelete='cascade'))
     website_link = db.relationship('WebsiteLink', backref=db.backref('images', lazy='dynamic'))
 
     def __init__(self, src, alt, link):
@@ -49,20 +49,43 @@ class WebsiteLink(db.Model):
     title = db.Column(db.String(300))
     status = db.Column(db.Integer)
     description = db.Column(db.String(500))
+    depth = db.Column(db.Integer)
+    size = db.Column(db.Integer)
 
-    website_id = db.Column(db.Integer, db.ForeignKey('website.id'))
+    website_id = db.Column(db.Integer, db.ForeignKey('website.id', ondelete='cascade'))
     website = db.relationship('Website', backref=db.backref('links', lazy='dynamic'))
 
-    def __init__(self, url, title, status, description, website):
+    def __init__(self, url, title, status, description, website, depth, size):
         self.url = url
         self.title = title
         self.status = status
         self.description = description
+        self.depth = depth
+        self.size = size
 
         self.website = website
 
     def __repr__(self):
         return '<Link %r>' % self.title
+
+class WebsiteParent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    child_url = db.Column(db.String(512))
+
+    parent_id = db.Column(db.Integer, db.ForeignKey('website_link.id', ondelete='cascade'))
+    parent = db.relationship('WebsiteLink', backref=db.backref('childs', lazy='dynamic'))
+
+    website_id = db.Column(db.Integer, db.ForeignKey('website.id', ondelete='cascade'))
+    website = db.relationship('Website', backref=db.backref('parents', lazy='dynamic')) # nullable=True
+
+    def __init__(self, parent, child_url, website):
+        self.parent = parent
+        self.child_url = child_url
+
+        self.website = website
+
+    def __repr__(self):
+        return '<Child  %r>' % self.child_url
 
 class Website(db.Model):
     id = db.Column(db.Integer, primary_key=True)
