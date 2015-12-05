9aaff5144e1e4816c8ff1c08a3a07bc7b32381a3
Ahmed Saidi <justroftest@gmail.com>
2015-12-05 18:30:05 +0000

    Set default 'From' email to noreply@shopifiedapp.com

--- app/settings.py
+++ app/settings.py
@@ -139,3 +139,4 @@ EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "noreply@shopifiedapp.com"
