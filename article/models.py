9c3ef827a38ef73e4806a0aa2cd721216f373848
Ahmed Saidi <justroftest@gmail.com>
2016-01-29 11:00:37 +0000

    Add Beta Elite to Sidebar Links

--- article/models.py
+++ article/models.py
@@ -11,12 +11,15 @@ PUBLISH_STAT = (
     (2, 'Waitting review'),
 )
 
-PLAN_CHOICES = (('64543a8eb189bae7f9abc580cfc00f76', 'Vip Elite'),
-              ('3eccff4f178db4b85ff7245373102aec', 'Elite'),
-              ('b17d8eacbb02bb907c2ccc854f7c282d', 'Team Shopify'),
-              ('55cb8a0ddbc9dacab8d99ac7ecaae00b', 'Pro'),
-              ('2877056b74f4683ee0cf9724b128e27b', 'Basic'),
-              ('606bd8eb8cb148c28c4c022a43f0432d', 'Free'))
+PLAN_CHOICES = (
+    ('3fd1d6a2ee2d8154874204a08b31575f', 'Beta Elite'),
+    ('64543a8eb189bae7f9abc580cfc00f76', 'Vip Elite'),
+    ('3eccff4f178db4b85ff7245373102aec', 'Elite'),
+    ('b17d8eacbb02bb907c2ccc854f7c282d', 'Team Shopify'),
+    ('55cb8a0ddbc9dacab8d99ac7ecaae00b', 'Pro'),
+    ('2877056b74f4683ee0cf9724b128e27b', 'Basic'),
+    ('606bd8eb8cb148c28c4c022a43f0432d', 'Free')
+)
 
 
 class Article(models.Model):
