f215d5b56a67d2090938224811f5bcae548b0efb
Ahmed Saidi <justroftest@gmail.com>
2016-01-29 01:52:18 +0000

    Sidebar links permission

--- article/models.py
+++ article/models.py
@@ -3,14 +3,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
-
-### TODO: Add validators
+from multiselectfield import MultiSelectField
 
 PUBLISH_STAT = (
     (0, 'Published'),
     (1, 'Draft'),
     (2, 'Waitting review'),
 )
+
+PLAN_CHOICES = (('64543a8eb189bae7f9abc580cfc00f76', 'Vip Elite'),
+              ('3eccff4f178db4b85ff7245373102aec', 'Elite'),
+              ('55cb8a0ddbc9dacab8d99ac7ecaae00b', 'Pro'),
+              ('2877056b74f4683ee0cf9724b128e27b', 'Basic'),
+              ('606bd8eb8cb148c28c4c022a43f0432d', 'Free'))
+
 
 class Article(models.Model):
     class Meta:
@@ -81,6 +87,7 @@ class SidebarLink(models.Model):
     icon = models.CharField(blank=True, default='', max_length=20)
 
     parent = models.ForeignKey('SidebarLink', on_delete=models.SET_NULL, related_name='childs', blank=True, null=True)
+    plans = MultiSelectField(choices=PLAN_CHOICES)
 
     def __str__(self):
         return self.title
