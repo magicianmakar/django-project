bf8b1b59a6c4305622c6b9fe38e5440a8d7782d3
Ahmed Saidi <justroftest@gmail.com>
2016-01-29 02:06:52 +0000

    Add Team Shopify plan

--- article/models.py
+++ article/models.py
@@ -13,6 +13,7 @@ PUBLISH_STAT = (
 
 PLAN_CHOICES = (('64543a8eb189bae7f9abc580cfc00f76', 'Vip Elite'),
               ('3eccff4f178db4b85ff7245373102aec', 'Elite'),
+              ('b17d8eacbb02bb907c2ccc854f7c282d', 'Team Shopify'),
               ('55cb8a0ddbc9dacab8d99ac7ecaae00b', 'Pro'),
               ('2877056b74f4683ee0cf9724b128e27b', 'Basic'),
               ('606bd8eb8cb148c28c4c022a43f0432d', 'Free'))
