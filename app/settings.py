1a6258b1b67104ca5ad6653af2439e342cb3af09
Ahmed Saidi <justroftest@gmail.com>
2016-02-08 17:46:11 +0000

    Enable Postgres if DATABASE_URL is defined

--- app/settings.py
+++ app/settings.py
@@ -106,10 +106,9 @@ USE_TZ = True
 SESSION_COOKIE_AGE = 6048000
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
-
-# Enable Connection Pooling (if desired)
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config()
+    DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
