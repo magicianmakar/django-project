71a73c1e7b094385066905922d3a954fd24a2c1b
Ahmed Saidi <justroftest@gmail.com>
2016-02-12 16:32:33 +0000

    Fix

--- app/storage.py
+++ app/storage.py
@@ -19,17 +19,9 @@ class CachedS3BotoStorage(S3BotoStorage):
         return name
 
 
+# We need a custom Storage Class for our Media files because we
+# want to have seperate locations for Static and Media files.
 class CachedMediaS3BotoStorage(S3BotoStorage):
-    """
-    S3 storage backend that saves the files locally, too.
-    """
     def __init__(self, *args, **kwargs):
         super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
         self.location = settings.MEDIAFILES_LOCATION
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
-
-    def save(self, name, content):
-        name = super(CachedS3BotoStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
