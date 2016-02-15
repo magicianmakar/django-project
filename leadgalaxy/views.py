befc11e20130f8e1abfb9d7bc2651b61e2d65139
Ahmed Saidi <justroftest@gmail.com>
2016-02-15 14:32:57 +0000

    Fix Adding aliexpress to the note

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -799,10 +799,10 @@ def api(request, target):
             store = ShopifyStore.objects.get(id=store, user=user)
             order_line = utils.get_shopify_order_line(store, order_id, line_id)
             if order_line:
-                note = 'Aliexpress Order ID: {} - {} / {}'.format(order_id, order_line['name'],
+                note = 'Aliexpress Order ID: {} - {} / {}'.format(source_id, order_line['name'],
                                                                   order_line['variant_title'])
             else:
-                note = 'Aliexpress Order ID: {}'.format(order_id)
+                note = 'Aliexpress Order ID: {}'.format(source_id)
 
             utils.add_shopify_order_note(store, order_id, note)
 
