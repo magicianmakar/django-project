b59643c519dbbb0e2d8244f8e929c18e32711581
Marian Gheata <victor960035@gmail.com>
2016-02-23 15:26:22 +0000

    Fix

--- leadgalaxy/utils.py
+++ leadgalaxy/utils.py
@@ -488,7 +488,7 @@ def product_changes_remap(changes):
     variants = changes['changes'].get('variants')
     if variants and len(variants):
         for i in variants:
-            if not i.get('changes'):
+            if not i or not i.get('changes'):
                 continue
 
             for change in i.get('changes'):
