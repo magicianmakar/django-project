1b77738ba9c8e8427b9cbb0dc352730184a963c9
Marian Gheata <victor960035@gmail.com>
2016-03-06 20:56:27 +0000

    Check if title and description is upper case

--- leadgalaxy/utils.py
+++ leadgalaxy/utils.py
@@ -797,6 +797,9 @@ class ProductFeed():
         if len(text) == 0:
             text = product.get('title', '')
 
+        if text.isupper():
+            text = text.title()
+
         return text
 
     def get_feed_stream(self):
