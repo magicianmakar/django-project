7fb9b917a3bb2ab85fa1f8b6fa8de0c357ed72ef
Marian Gheata <victor960035@gmail.com>
2016-03-07 23:59:25 +0000

    Improve updater

--- leadgalaxy/management/commands/fulfillment_status.py
+++ leadgalaxy/management/commands/fulfillment_status.py
@@ -13,11 +13,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.HTTP_INFO('* Get Orders Fulfillment Status'))
 
-        orders = ShopifyOrder.objects.filter(shopify_status='').exclude(store=None).order_by('store')
+        orders = ShopifyOrder.objects.filter(shopify_status='').exclude(store=None).order_by('store', 'order_id')
 
         self.stdout.write(self.style.HTTP_INFO('* Found %d Orders' % orders.count()))
 
         store_title = ''
+        self.order = {'id': 0}
         with transaction.atomic():
             for order in orders:
                 if store_title != order.store.title:
@@ -25,7 +26,7 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.MIGRATE_SUCCESS('Store {}'.format(store_title)))
 
                 try:
-                    line = utils.get_shopify_order_line(order.store, order.order_id, order.line_id)
+                    shopify_order, line = self.get_order_line(order.store, order.order_id, order.line_id)
                 except Exception as e:
                     self.stdout.write(self.style.ERROR('Exception: {}'.format(repr(e))))
                     self.stdout.write(self.style.ERROR('Order: {} / {}'.format(order.order_id, order.line_id)))
@@ -36,8 +37,29 @@ class Command(BaseCommand):
                 if not fulfillment_status:
                     fulfillment_status = ''
 
-                self.stdout.write(self.style.MIGRATE_SUCCESS(
-                    '{}/{} {}'.format(order.order_id, order.line_id, fulfillment_status)))
+                self.stdout.write(self.style.HTTP_INFO(
+                    '{}/{} {} [Order: {}]'.format(order.order_id, order.line_id, fulfillment_status, shopify_order.get('fulfillment_status', ''))))
 
                 order.shopify_status = line.get('fulfillment_status', '')
                 order.save()
+
+    def get_order(self, store, order_id):
+        rep = requests.get(store.get_link('/admin/orders/{}.json'.format(order_id), api=True))
+        data = rep.json()
+        if 'order' in data:
+            return data['order']
+        else:
+            raise Exceprion(rep.text)
+
+    def get_order_line(self, store, order_id, line_id):
+        if int(self.order['id']) != int(order_id):
+            order = self.get_order(store, order_id)
+        else:
+            order = self.order
+        if order:
+            self.order = order
+            for line in order['line_items']:
+                if int(line['id']) == int(line_id):
+                    return order, line
+
+        return None
