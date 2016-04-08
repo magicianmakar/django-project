75838da460905cfb5372ff7c76edc769265f46ca
Marian Gheata <victor960035@gmail.com>
2016-04-08 11:10:46 +0000

    Fix

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -508,7 +508,7 @@ def proccess_api(request, user, method, target, data):
         except ShopifyStore.DoesNotExist:
             return JsonResponse({'error': 'Store not found'}, status=404)
 
-        url = stor.get_link(data.get('url'), api=True)
+        url = store.get_link(data.get('url'), api=True)
         requests.put(url, json=json.loads(data.get('data')))
 
         return JsonResponse({
