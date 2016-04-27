87567078ffb89b334bc5c85541ca8e71103b1f41
Marian Gheata <victor960035@gmail.com>
2016-04-27 16:04:45 +0000

    Orders Admin page optimiztion

--- shopify_orders/admin.py
+++ shopify_orders/admin.py
@@ -9,13 +9,16 @@ from shopify_orders.models import ShopifySyncStatus, ShopifyOrder, ShopifyOrderL
 class ShopifySyncStatusAdmin(admin.ModelAdmin):
     list_display = ('store', 'sync_type', 'sync_status', 'created_at', 'updated_at')
     list_filter = ('sync_type', 'sync_status')
+    raw_id_fields = ('store',)
 
 
 @admin.register(ShopifyOrder)
 class ShopifyOrderApiAdmin(admin.ModelAdmin):
     list_display = ('order_id', 'financial_status', 'fulfillment_status', 'created_at', 'updated_at')
+    raw_id_fields = ('user', 'store')
 
 
 @admin.register(ShopifyOrderLine)
 class ShopifyOrderLineAdmin(admin.ModelAdmin):
     list_display = ('line_id', 'order', 'title', 'variant_title')
+    raw_id_fields = ('product', 'order')
