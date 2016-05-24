b15315a560385027d44e934805e675573ad41ae5
Marian Gheata <victor960035@gmail.com>
2016-05-24 19:12:19 +0000

    Duplicate entries

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-export DATABASE_URL=postgres://postgres:@postgres:5432/python-test-app
+export DATABASE_URL="postgres://postgres:@postgres:5432/python-test-app"
 export DEBUG_APP="TRUE"
 export SENDGRID_PASSWORD=""
 export SENDGRID_USERNAME=""
 export S3_BUCKET_NAME='shopifiedapp'
-export AWS_ACCESS_KEY_ID=''
-export AWS_SECRET_ACCESS_KEY=''
-export JVZOO_SECRET=''
-export REDIS_URL="redis://127.0.0.1:6379"
-export REDISCLOUD_URL="redis://127.0.0.1:6379"
-export DEBUG_APP="TRUE"
-export SENDGRID_PASSWORD=""
-export SENDGRID_USERNAME=""
-export S3_BUCKET_NAME='shopifiedapp'
-export AWS_ACCESS_KEY_ID=''
-export AWS_SECRET_ACCESS_KEY=''
-export JVZOO_SECRET=''
-export REDIS_URL="redis://127.0.0.1:6379"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export JVZOO_SECRET=""
 export REDISCLOUD_URL="redis://127.0.0.1:6379"
 export REDISCLOUD_CACHE="redis://127.0.0.1:6379"
 
