b1e4d8b42cba29952ece8b2ec72bd2127ea928ad
Marian Gheata <victor960035@gmail.com>
2016-05-30 15:45:33 +0000

    Fix exporting product with more than two variants
    Ref d1681e8

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,19 +4,27 @@
 var taskIntervals = {};
 var taskCallsCount = {};
 
-function allPossibleCases(arr) {
-  if (arr.length == 1) {
-    return arr[0];
-  } else {
-    var result = [];
-    var allCasesOfRest = allPossibleCases(arr.slice(1));  // recur with the rest of array
-    for (var i = 0; i < allCasesOfRest.length; i++) {
-      for (var j = 0; j < arr[0].length; j++) {
-        result.push([arr[0][j], allCasesOfRest[i]]);
-      }
+function allPossibleCases(arr, top) {
+    top = typeof(top) === 'undefined' ? true : top;
+
+    var sep = new Array(10).join('-');
+    if (arr.length === 0) {
+        return [];
+    } else if (arr.length === 1) {
+        return arr[0];
+    } else {
+        var result = [];
+        var allCasesOfRest = allPossibleCases(arr.slice(1), false);
+        for (var c in allCasesOfRest) {
+            for (var i = 0; i < arr[0].length; i++) {
+                result.push(arr[0][i] + sep + allCasesOfRest[c]);
+            }
+        }
+
+        return top ? result.map(function(i) {
+            return i.split(sep);
+        }) : result;
     }
-    return result;
-  }
 }
 
 function displayAjaxError(desc, data) {
