1abd9a530e62d2ee80a9b79bec319f3ec1bf9062
Marian Gheata <victor960035@gmail.com>
2016-06-13 11:55:10 +0000

    Default Plan Test

--- leadgalaxy/tests/tests_models.py
+++ leadgalaxy/tests/tests_models.py
@@ -14,3 +14,16 @@ class UserTestCase(TestCase):
 
         self.assertIsNotNone(user.profile)
         self.assertIsNotNone(user.profile.plan)
+
+    def test_userprofile_signal_with_default_plan(self):
+        GroupPlan.objects.create(title='Pro Plan', slug='pro-plan', default_plan=0)
+        GroupPlan.objects.create(title='Free Plan', slug='free-plan', default_plan=1)
+
+        user = User.objects.create_user(username='john',
+                                        email='john.test@gmail.com',
+                                        password='123456')
+
+        self.assertIsNotNone(user.profile)
+        self.assertIsNotNone(user.profile.plan)
+
+        self.assertEqual(user.profile.plan.slug, 'free-plan')
