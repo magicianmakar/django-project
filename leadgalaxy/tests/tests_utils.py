8c36e7c9c9132f6b969543f40f604d5f15b221aa
Marian Gheata <victor960035@gmail.com>
2016-06-14 11:22:57 +0000

    Pixlr: Add DHGate to allowed webstites

--- leadgalaxy/tests/tests_utils.py
+++ leadgalaxy/tests/tests_utils.py
@@ -314,6 +314,7 @@ class UtilsTestCase(TestCase):
         self.assertTrue(utils.upload_from_url('https://cdn.shopify.com/s/files/1/1013/1174/products/Fashion-Metal---magnification-wholesale.jpeg'))
         self.assertTrue(utils.upload_from_url('https://cdn.shopify.com/s/files/1/1013/1174/products/Fashion-Metal---magnification-wholesale.jpeg?v=1452639314'))
         self.assertTrue(utils.upload_from_url('http://ecx.images-amazon.com/images/I/21iTIaYnSzL.jpg'))
+        self.assertTrue(utils.upload_from_url('http://www.dhresource.com/0x0/f2/albu/g2/M00/DF/F1/rBVaGlUVIdWAIH4VAASbr7-6PiQ259.jpg'))
 
         self.assertTrue(utils.upload_from_url('http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/'
                                               '2016/04/08/source-img/20160408155854_48756.jpg',
