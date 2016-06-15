ba25e93b786c3b1a3d5ff203b10b8dabfaa11dce
Marian Gheata <victor960035@gmail.com>
2016-06-15 15:29:50 +0000

    Unused template filters

--- leadgalaxy/templatetags/perms_helper.py
+++ leadgalaxy/templatetags/perms_helper.py
@@ -18,18 +18,3 @@ def can(user, perm_name):
             return user.can(perm_name)
 
     return False
-
-@register.filter(name='in_groups')
-def in_groups(user, groups_name):
-    """
-    Usage in template:
-    {% if request.user|in_groups:'galaxy_admin,galaxy_market' %}
-        In galaxy_admin or galaxy_market Group
-    {% endif %}
-    """
-
-    for i in groups_name.split(','):
-        if in_group(user, i):
-            return True
-
-    return False
--- leadgalaxy/templatetags/template_helper.py
+++ leadgalaxy/templatetags/template_helper.py
@@ -9,32 +9,6 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def render_project(context, tpl, project):
-    template = Template("{% load template_helper %}\n" + tpl)
-    ctx = Context({
-        'project': project,
-        'template': project.template,
-        'metrics': project.get_metrics('all'),
-    })
-
-    return template.render(ctx)
-
-
-@register.simple_tag(takes_context=True)
-def render_category(context, tpl, category):
-    template = Template("{% load template_helper %}\n" + tpl)
-    ctx = Context({
-        'project': category.project,
-        'template': category.project.template,
-        'category': category,
-        'metrics': category.project.get_metrics('all'),
-
-    })
-
-    return template.render(ctx)
-
-
-@register.simple_tag(takes_context=True)
 def encode_order(context, data, auto):
     data['auto'] = (auto == 'True')
     return json.dumps(data).encode('base64').replace('\n', '')
