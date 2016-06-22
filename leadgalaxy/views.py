299ffeac9d0eec67658a2d2b3427a8c16340c52a
Marian Gheata <victor960035@gmail.com>
2016-06-22 20:41:07 +0000

    Fix for Puerto Rico addresses

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -3148,8 +3148,15 @@ def orders_view(request):
                             shipping_address_asci[u'province'] = province
                         else:
                             shipping_address_asci[u'province'] = shipping_address_asci[u'country_code']
+
                     elif shipping_address_asci[u'province'] == 'Washington DC':
                         shipping_address_asci[u'province'] = u'Washington'
+
+                    elif shipping_address_asci['province'] == 'Puerto Rico':
+                        # Puerto Rico is a country in Aliexpress
+                        shipping_address_asci['province'] == 'PR'
+                        shipping_address_asci['country_code'] == 'PR'
+                        shipping_address_asci['country'] == 'Puerto Rico'
 
                     phone = shipping_address_asci.get('phone')
                     if not phone or models_user.get_config('order_default_phone') != 'customer':
