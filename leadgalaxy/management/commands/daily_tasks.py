2060db3c15ee4bdab1e96ead538fc84c77bd3180
Marian Gheata <victor960035@gmail.com>
2016-06-22 12:24:26 +0000

    Handle fulfillments API errors
    This add handling of: API rate limit, already fulfilled and other excptions
    that weren't handled before, resulting in many non-fulfilled and with a
    tracking number orders in Tracking page

--- leadgalaxy/management/commands/daily_tasks.py
+++ leadgalaxy/management/commands/daily_tasks.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 import requests
+import time
+from simplejson import JSONDecodeError
 
 from leadgalaxy.models import *
 from leadgalaxy import utils
@@ -41,13 +43,18 @@ class Command(BaseCommand):
         # Auto fulfill (Daily)
         time_threshold = timezone.now() - timezone.timedelta(days=1)
         orders = ShopifyOrderTrack.objects.exclude(shopify_status='fulfilled').exclude(source_tracking='') \
-                                     .filter(status_updated_at__lt=time_threshold) \
-                                     .order_by('store', 'status_updated_at')
+                                          .filter(status_updated_at__lt=time_threshold) \
+                                          .order_by('store', 'status_updated_at')
 
         print 'Orders Count (daily):', orders.count()
 
         users = {}
-        count = 0
+        counter = {
+            'fulfilled': 0,
+            'need_fulfill': 0,
+            'ignored_orders': 0,
+        }
+
         self.store_countdown = {}
 
         for order in orders:
@@ -56,24 +63,32 @@ class Command(BaseCommand):
             else:
                 user = order.store.user
 
-            if not user or user.get_config('auto_shopify_fulfill') != 'daily':
+            fulfill_option = user.get_config('auto_shopify_fulfill') if user else None
+            if not user or fulfill_option != 'daily':
                 users[order.store_id] = False
+                counter['ignored_orders'] += 1
                 continue
             else:
                 users[order.store_id] = user
 
             try:
+                counter['need_fulfill'] += 1
+
                 if self.fulfill_order(order, order.store, user):
                     order.shopify_status = 'fulfilled'
                     order.auto_fulfilled = True
                     order.save()
 
-                    count += 1
-                    if count % 50 == 0:
-                        print 'Fulfill Progress: %d' % count
+                    counter['fulfilled'] += 1
+                    if counter['fulfilled'] % 50 == 0:
+                        print 'Fulfill Progress: %d' % counter['fulfilled']
 
             except:
                 raven_client.captureException()
+
+        print 'Fulfilled Orders:', counter['fulfilled']
+        print 'Need Fulfill Orders:', counter['need_fulfill']
+        print 'Ignored Orders:', counter['ignored_orders']
 
     def profile_changed(self, profile, expired_plan, new_plan):
         data = {
@@ -91,12 +106,53 @@ class Command(BaseCommand):
     def fulfill_order(self, order, store, user):
         api_data = utils.order_track_fulfillment(order_track=order, user_config=user.get_config())
 
-        rep = requests.post(
-            url=store.get_link('/admin/orders/{}/fulfillments.json'.format(order.order_id), api=True),
-            json=api_data
-        )
+        fulfilled = False
+        tries = 3
 
-        fulfilled = 'fulfillment' in rep.json()
+        while tries > 0:
+            try:
+                rep = requests.post(
+                    url=store.get_link('/admin/orders/{}/fulfillments.json'.format(order.order_id), api=True),
+                    json=api_data
+                )
+
+                rep.raise_for_status()
+
+                fulfilled = 'fulfillment' in rep.json()
+                break
+
+            except (JSONDecodeError, requests.exceptions.ConnectTimeout):
+                print 'Sleep For 2 sec'
+                time.sleep(2)
+                continue
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 429:
+                    # Wait and retry
+                    print 'Sleep For 5 sec'
+                    time.sleep(5)
+                    continue
+
+                elif e.response.status_code == 422:
+                    if 'is already fulfilled' in rep.text:
+                        # Mark as fulfilled but not auto-fulfilled
+                        print 'Already fulfilled #{} in [{}]'.format(order.order_id, order.store.title)
+                        order.shopify_status = 'fulfilled'
+                        order.save()
+                        return False
+
+                elif e.response.status_code == 404:
+                    print 'Not found #{} in [{}]'.format(order.order_id, order.store.title)
+                    order.hidden = True
+                    order.save()
+                    return False
+
+                raven_client.captureException()
+
+            except:
+                raven_client.captureException()
+            finally:
+                tries -= 1
+
         if fulfilled:
             note = "Auto Fulfilled by Shopified App (Line Item #{})".format(order.line_id)
 
@@ -104,5 +160,7 @@ class Command(BaseCommand):
             tasks.add_ordered_note.apply_async(args=[store.id, order.order_id, note], countdown=countdown)
 
             self.store_countdown[store.id] = countdown + 5
+        else:
+            raven_client.captureMessage('Order Was not fulfilled', extra={'order': order.id}, level='warning')
 
         return fulfilled
