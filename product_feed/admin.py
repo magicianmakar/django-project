e80445ee7ddcf0519e7982d4d4932cfee4bd6f45
Marian Gheata <victor960035@gmail.com>
2016-07-05 12:42:05 +0000

    Misc

--- product_feed/admin.py
+++ product_feed/admin.py
@@ -5,5 +5,6 @@ from .models import FeedStatus
 
 @admin.register(FeedStatus)
 class FeedStatusAdmin(admin.ModelAdmin):
-    list_display = ('store', 'status', 'created_at', 'updated_at')
+    list_display = ('store', 'status', 'generation_time', 'fb_access_at', 'updated_at')
+    raw_id_fields = ('store',)
     exclude = []
