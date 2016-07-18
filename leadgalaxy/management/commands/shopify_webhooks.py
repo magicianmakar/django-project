ad9cb974ee59560acd7f48f012f1983d1d71dde3
Marian Gheata <victor960035@gmail.com>
2016-07-18 18:12:10 +0000

    Add Permission argument to webhook command

--- leadgalaxy/management/commands/shopify_webhooks.py
+++ leadgalaxy/management/commands/shopify_webhooks.py
@@ -12,17 +12,16 @@ class Command(BaseCommand):
 
         parser.add_argument('--plan', dest='plan_id', action='append', type=int, help='Plan ID')
         parser.add_argument('--store', dest='store_id', action='append', type=int, help='Store ID')
+        parser.add_argument('--permission', dest='permission', action='append', type=str, help='Users with permission')
         parser.add_argument('--delete_on_detach', dest='delete_on_detach',
                             action='store_true', help='Delete Saved Webhook on detach')
 
     def handle(self, *args, **options):
         action = options['action'][0]
 
-        if not options['plan_id']:
-            options['plan_id'] = []
-
-        if not options['store_id']:
-            options['store_id'] = []
+        for i in ['plan_id', 'store_id', 'permission']:
+            if not options[i]:
+                options[i] = []
 
         for plan_id in options['plan_id']:
             try:
@@ -37,6 +36,21 @@ class Command(BaseCommand):
             self.stdout.write(self.style.HTTP_INFO('Stores count: %d' % stores.count()))
             for store in stores:
                 self.handle_store(store, action, options['delete_on_detach'])
+
+        for permission in options['permission']:
+            for p in AppPermission.objects.filter(name=permission):
+                for plan in p.groupplan_set.all():
+                    self.write_success(u'{} webhooks for Plan: {}'.format(action.title(), plan.title))
+
+                    for profile in plan.userprofile_set.select_related('user').all():
+                        for store in profile.get_active_stores():
+                            self.handle_store(store, action, options['delete_on_detach'])
+
+                for bundle in p.featurebundle_set.all():
+                    self.write_success(u'{} webhooks for Bundle: {}'.format(action.title(), bundle.title))
+                    for profile in bundle.userprofile_set.select_related('user').all():
+                        for store in profile.get_active_stores():
+                            self.handle_store(store, action, options['delete_on_detach'])
 
         for store_id in options['store_id']:
             try:
@@ -56,3 +70,6 @@ class Command(BaseCommand):
         else:
             webhooks = utils.detach_webhooks(store, delete_on_detach)
             self.stdout.write(self.style.MIGRATE_SUCCESS('    - {}'.format(store.title)))
+
+    def write_success(self, message):
+        self.stdout.write(self.style.MIGRATE_SUCCESS(message))
