540e0d02c33760deaf0c79e8202eac25caad75f3
Marian Gheata <victor960035@gmail.com>
2016-07-19 15:51:14 +0000

    Fix

--- article/urls.py
+++ article/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('article.views',
     url(r'^$', 'index'),
     url(r'^tagged/(?P<tag>[a-z-]+)$$', 'index', name="tagged"),
     url(r'^view/(?P<id_article>\d+)$', 'view'),
-    url(r'^view/(?P<slug_article>[a-zA-Z0-9-]+)$', 'view'),
+    url(r'^view/(?P<slug_article>[a-zA-Z0-9-_]+)$', 'view'),
     url(r'^edit/(?P<article_id>\d+)$', 'edit', name="edit"),
     url(r'^add$', 'submit', name="add-page"),
 
