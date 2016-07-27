be8791b773c969c105accb8437ce9bddf59262a9
Marian Gheata <victor960035@gmail.com>
2016-07-27 12:07:22 +0000

    Add store to the cache key

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -1845,7 +1845,7 @@ def webhook(request, provider, option):
                 return JsonResponse({'status': 'ok', 'warning': 'Non-handled Topic'})
 
             if topic == 'products/update':
-                countdown_key = 'eta_product_{}'.format(shopify_product['id'])
+                countdown_key = 'eta_product_{}_{}'.format(store.id, shopify_product['id'])
                 if cache.get(countdown_key) is None:
                     cache.set(countdown_key, True, timeout=5)
                     tasks.update_shopify_product.apply_async(args=[store.id, shopify_product['id']], countdown=5)
@@ -1876,7 +1876,7 @@ def webhook(request, provider, option):
                 ShopifyWebhook.objects.filter(token=token, store=store, topic=topic) \
                                       .update(call_count=F('call_count')+1, updated_at=timezone.now())
 
-                countdown_key = 'eta_order_{}'.format(shopify_order['id'])
+                countdown_key = 'eta_order_{}_{}'.format(store.id, shopify_order['id'])
                 if cache.get(countdown_key) is None:
                     cache.set(countdown_key, True, timeout=5)
                     tasks.update_shopify_order.apply_async(args=[store.id, shopify_order['id']], countdown=5)
