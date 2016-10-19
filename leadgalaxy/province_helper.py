a4aaef8bf94345bbbe148de57ed06a500a9e6883
Marian Gheata <victor960035@gmail.com>
2016-10-19 21:21:05 +0000

    Add More UK cities

--- leadgalaxy/province_helper.py
+++ leadgalaxy/province_helper.py
@@ -6,13 +6,14 @@ from django.conf import settings
 
 
 def load_uk_provincess():
-    data_file = os.path.join(settings.BASE_DIR, 'app', 'data', 'uk_provinces.csv')
-    uk_provinces = {}
-    lines = open(data_file).readlines()
-    for l in lines:
-        l = l.split('|')
-        if len(l) == 2:
-            uk_provinces[l[0]] = l[1].strip()
+    for i in ['uk_provinces2.csv', 'uk_provinces.csv']:
+        data_file = os.path.join(settings.BASE_DIR, 'app', 'data', i)
+        uk_provinces = {}
+        lines = open(data_file).readlines()
+        for l in lines:
+            l = l.split('|')
+            if len(l) == 2:
+                uk_provinces[l[0].lower().strip()] = l[1].strip()
 
     return uk_provinces
 
