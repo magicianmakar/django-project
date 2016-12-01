2b15a9fe609cbd849344fb054e87b91eb2954693
Marian Gheata <victor960035@gmail.com>
2016-12-01 22:22:55 +0000

    Revert "Disable Orders update for in development "
    This reverts commit fe7d52462e2a92401349eff8e4a6c6244ccc21b0.

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -1636,11 +1636,10 @@ def proccess_api(request, user, method, target, data):
                 line_id=line_id
             ).update(track=track)
 
-            if not settings.DEBUG:
-                # TODO: make this done with one api call
-                tasks.mark_as_ordered_note.apply_async(
-                    args=[store.id, order_id, line_id, source_id],
-                    countdown=note_delay)
+            # TODO: make this done with one api call
+            tasks.mark_as_ordered_note.apply_async(
+                args=[store.id, order_id, line_id, source_id],
+                countdown=note_delay)
 
             store.pusher_trigger('order-source-id-add', {
                 'track': track.id,
@@ -4149,7 +4148,7 @@ def orders_view(request):
             # Update outdated order data by comparing last update timestamp
             countdown = 1
             for order in page:
-                if arrow.get(order['updated_at']).timestamp > order['db_updated_at'] and not settings.DEBUG:
+                if arrow.get(order['updated_at']).timestamp > order['db_updated_at']:
                     tasks.update_shopify_order.apply_async(
                         args=[store.id, order['id']],
                         kwarg={'shopify_order': order, 'from_webhook': False},
