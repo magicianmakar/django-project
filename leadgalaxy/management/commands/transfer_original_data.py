f0da4d8abb2e37123a43153f8f01d4d38a128a60
Marian Gheata <victor960035@gmail.com>
2016-12-04 13:50:19 +0000

    Update migration command

--- leadgalaxy/management/commands/transfer_original_data.py
+++ leadgalaxy/management/commands/transfer_original_data.py
@@ -3,20 +3,38 @@ from django.db import transaction
 
 from leadgalaxy.models import ShopifyProduct
 
+from tqdm import tqdm
+
 
 class Command(BaseCommand):
     help = 'Transfers original data from products to the data store'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--max', dest='max', action='store', required=True, type=int,
+            help='Maximuim number of products to migrate')
+
     def handle(self, *args, **options):
-        self.write_success('Starting...')
-        products = ShopifyProduct.objects.filter(original_data_key=None)[:10000]
-        self.write_success('Transferring {} product(s).'.format(products.count()))
-        for product in products:
-            self.write_success('Transferring product with ID: {}'.format(product.id))
-            with transaction.atomic():
+
+        products = ShopifyProduct.objects.filter(original_data_key=None)[:options['max']]
+        total = products.count()
+
+        self.write_success('Transferring {} products'.format(products.count()))
+
+        pbar = tqdm(total=total)
+
+        with transaction.atomic():
+            for product in products:
                 product.set_original_data(product.original_data)
-            self.write_success('Transfer successful.')
-        self.write_success('Done.')
+                pbar.update(1)
+
+        pbar.close()
+
+        if total < 10:
+            self.stdout.write('Products: ')
+            self.stdout.write('\n'.join(['\thttps://app.shopifiedapp.com/product/%d' % p.id for p in products]))
+
+        self.write_success('Done')
 
     def write_success(self, message):
         self.stdout.write(self.style.MIGRATE_SUCCESS(message))
