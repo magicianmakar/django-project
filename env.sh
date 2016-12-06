6f7786255a6122c26f77ec0561366f4a32980637
billyswift <billyswifty@gmail.com>
2016-12-06 15:35:08 -0600

    add env.sh file

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DATABASE_URL="localhost://localhost:@root:5432/python-test-app"
+export DEBUG_APP="TRUE"
+export SENDGRID_PASSWORD=""
+export SENDGRID_USERNAME=""
+export S3_BUCKET_NAME='shopifiedapp-assets'
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export JVZOO_SECRET=""
+export REDISCLOUD_URL="redis://:"
+export REDISCLOUD_CACHE="redis://:"
+export STRIPE_PUBLIC_KEY=""
+export STRIPE_SECRET_KEY=""
+export SHOPIFY_API_KEY=""
+export SHOPIFY_API_SECRET=""
+
