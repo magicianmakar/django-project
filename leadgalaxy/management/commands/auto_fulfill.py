3e3dc9dd02282d07c1df40e3e8e270e5413732da
Marian Gheata <victor960035@gmail.com>
2016-12-13 13:40:35 +0000

    Auto fulfill command

---
+++ leadgalaxy/management/commands/auto_fulfill.py
@@ -0,0 +1,159 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+import requests
+import time
+from simplejson import JSONDecodeError
+
+from leadgalaxy.models import *
+from leadgalaxy import utils
+from leadgalaxy import tasks
+
+from raven.contrib.django.raven_compat.models import client as raven_client
+
+
+class Command(BaseCommand):
+    help = 'Auto fulfill tracked orders with a tracking number and unfulfilled status'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--threshold', dest='threshold', action='store', type=int,
+            help='Fulfill orders updated before threshold (seconds)')
+
+        parser.add_argument(
+            '--store', dest='store', action='store', type=int,
+            help='Fulfill orders for the given store')
+
+    def handle(self, *args, **options):
+        try:
+            self.start_command(*args, **options)
+        except:
+            raven_client.captureException()
+
+    def write(self, msg, style_func=None, ending=None):
+        self.stdout.write(msg, style_func, ending)
+
+    def start_command(self, *args, **options):
+        threshold = options.get('threshold')
+        fulfill_store = options.get('store')
+
+        if threshold is None:
+            threshold = 60
+
+        time_threshold = timezone.now() - timezone.timedelta(seconds=threshold)
+        orders = ShopifyOrderTrack.objects.exclude(shopify_status='fulfilled') \
+                                          .exclude(source_tracking='') \
+                                          .exclude(hidden=True) \
+                                          .filter(status_updated_at__lt=time_threshold) \
+                                          .filter(store__is_active=True) \
+                                          .filter(store__auto_fulfill__in=['hourly', 'daily', 'enable']) \
+                                          .order_by('id')
+
+        if fulfill_store is not None:
+            orders = orders.filter(store=fulfill_store)
+
+        self.write('Auto Fulfill %d Orders' % orders.count(), self.style.HTTP_INFO)
+
+        counter = {
+            'fulfilled': 0,
+            'need_fulfill': 0,
+        }
+
+        self.store_countdown = {}
+
+        for order in orders:
+            try:
+                counter['need_fulfill'] += 1
+
+                if self.fulfill_order(order):
+                    order.shopify_status = 'fulfilled'
+                    order.auto_fulfilled = True
+                    order.save()
+
+                    counter['fulfilled'] += 1
+                    if counter['fulfilled'] % 50 == 0:
+                        self.write('Fulfill Progress: %d' % counter['fulfilled'])
+
+            except:
+                raven_client.captureException()
+
+        self.write('Fulfilled Orders: {} / {}'.format(
+            counter['fulfilled'], counter['need_fulfill']))
+
+    def fulfill_order(self, order):
+        store = order.store
+        user = store.user
+
+        api_data = utils.order_track_fulfillment(order_track=order, user_config=user.get_config())
+
+        fulfilled = False
+        tries = 3
+
+        while tries > 0:
+            try:
+                rep = requests.post(
+                    url=store.get_link('/admin/orders/{}/fulfillments.json'.format(order.order_id), api=True),
+                    json=api_data
+                )
+
+                rep.raise_for_status()
+
+                fulfilled = 'fulfillment' in rep.json()
+                break
+
+            except (JSONDecodeError, requests.exceptions.ConnectTimeout):
+                self.write('Sleep for 2 sec')
+                time.sleep(2)
+                continue
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 429:
+                    # Wait and retry
+                    self.write('Sleep for 5 sec')
+                    time.sleep(5)
+                    continue
+
+                elif e.response.status_code == 422:
+                    if 'is already fulfilled' in rep.text:
+                        # Mark as fulfilled but not auto-fulfilled
+                        self.write(u'Already fulfilled #{} in [{}]'.format(order.order_id, order.store.title))
+                        order.shopify_status = 'fulfilled'
+                        order.save()
+                        return False
+
+                    elif 'invalid for this fulfillment service' in rep.text:
+                        # Using a different fulfillment_service (i.e: amazon_marketplace_web)
+                        self.write(u'Invalid for this fulfillment service #{} in [{}]'.format(order.order_id, order.store.title))
+                        order.shopify_status = 'fulfilled'
+                        order.save()
+                        return False
+
+                elif e.response.status_code == 404:
+                    self.write(u'Not found #{} in [{}]'.format(order.order_id, order.store.title))
+                    order.delete()
+
+                    return False
+
+                elif e.response.status_code == 402:
+                    order.hidden = True
+                    order.save()
+
+                    return False
+
+                raven_client.captureException(extra={'order_track': order.id, 'response': rep.text})
+
+            except:
+                raven_client.captureException()
+            finally:
+                tries -= 1
+
+        if fulfilled:
+            note = "Auto Fulfilled by Shopified App (Line Item #{})".format(order.line_id)
+
+            countdown = self.store_countdown.get(store.id, 30)
+            tasks.add_ordered_note.apply_async(args=[store.id, order.order_id, note], countdown=countdown)
+
+            self.store_countdown[store.id] = countdown + 5
+        else:
+            raven_client.captureMessage('Order Was not fulfilled', extra={'order': order.id}, level='warning')
+
+        return fulfilled
---
+++ leadgalaxy/tests/test_auto_fulfill.py
@@ -0,0 +1,118 @@
+import simplejson as json
+
+from django.core.cache import cache
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
+from django.core.management import call_command
+
+from leadgalaxy import utils
+from leadgalaxy.models import (
+    AliexpressProductChange,
+    ShopifyOrderTrack,
+    ShopifyProduct,
+    ShopifyStore,
+    ShopifyProductExport,
+    UserProfile,
+    GroupPlan
+)
+
+import factory
+
+from mock import patch, Mock
+
+
+class ShopifyOrderTrackFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = ShopifyOrderTrack
+        django_get_or_create = ['line_id', 'order_id', 'user_id']
+
+    line_id = '1654811'
+    order_id = '5415135175'
+    source_tracking = 'MA7565915257226HK'
+    user_id = 1
+    store_id = 1
+    status_updated_at = timezone.now()
+
+
+class ShopifyStoreFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = utils.ShopifyStore
+        django_get_or_create = ['api_url']
+
+    title = 'uncommonnow'
+    api_url = 'https://:88937df17024aa5126203507e2147f47@shopified-app-ci.myshopify.com'
+    user_id = 1
+    auto_fulfill = 'enable'
+
+
+class ShopifyOrderFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = utils.ShopifyOrder
+        django_get_or_create = ['order_id']
+
+    order_id = '5415135175'
+    country_code = 'US'
+    user_id = 1
+    store_id = 1
+    order_number = 31
+    total_price = 100
+    customer_id = 1
+    created_at = utils.timezone.now()
+    updated_at = utils.timezone.now()
+
+
+class ShopifyOrderLineFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = utils.ShopifyOrderLine
+        django_get_or_create = ['line_id']
+
+    line_id = '1654811'
+    shopify_product = 10000
+    price = 10
+    quantity = 100
+    variant_id = 12345
+    order = factory.SubFactory(ShopifyOrderFactory)
+
+
+class AutoFulfillTestCase(TestCase):
+
+    def setUp(self):
+        self.store = ShopifyStoreFactory()
+
+    @patch('leadgalaxy.management.commands.auto_fulfill.Command.write', Mock())
+    @patch('leadgalaxy.management.commands.auto_fulfill.Command.fulfill_order')
+    def test_fulfill_tracked_order(self, fulfill_order):
+        track = ShopifyOrderTrackFactory(
+            order_id='5415135170',
+            line_id='1654810',
+            source_tracking='MA7565915257226HK',
+        )
+
+        track.status_updated_at = timezone.now() - timezone.timedelta(seconds=61)
+        track.save()
+
+        call_command('auto_fulfill')
+
+        fulfill_order.assert_called_with(track)
+
+    @patch('leadgalaxy.management.commands.auto_fulfill.Command.write', Mock())
+    @patch('leadgalaxy.management.commands.auto_fulfill.Command.fulfill_order')
+    def test_fulfill_ignore_no_tracking_number(self, fulfill_order):
+        track = ShopifyOrderTrackFactory(
+            order_id='5415135170',
+            line_id='1654810',
+            source_tracking='',
+        )
+
+        track.status_updated_at = timezone.now() - timezone.timedelta(seconds=61)
+        track.save()
+
+        call_command('auto_fulfill')
+
+        fulfill_order.assert_not_called()
