91a292db7094e271d7dc5ff4b4a2386704169d21
Marian Gheata <victor960035@gmail.com>
2016-12-20 15:54:39 +0000

    Change test database names

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DATABASE_URL="postgres://postgres:@postgres:5432/python-test-app"
-export DATA_STORE_DATABASE_URL="postgres://postgres:@postgres:5432/python-test-app"
+export DATABASE_URL="postgres://postgres:@postgres:5432/default-db"
+export DATA_STORE_DATABASE_URL="postgres://postgres:@postgres:5432/store-db"
 export DEBUG_APP="TRUE"
 export SENDGRID_PASSWORD=""
 export SENDGRID_USERNAME=""
