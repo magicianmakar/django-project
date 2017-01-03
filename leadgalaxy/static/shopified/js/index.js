2ee1841fad497c63c301607e70fd6d5c04020b10
Marian Gheata <victor960035@gmail.com>
2017-01-03 21:54:23 +0000

    Change Switchery color

--- leadgalaxy/static/shopified/js/index.js
+++ leadgalaxy/static/shopified/js/index.js
@@ -528,7 +528,7 @@ $(function () {
     var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
     elems.forEach(function(html) {
         var switchery = new Switchery(html, {
-            color: '#1AB394',
+            color: '#93c47d',
             size: 'small'
         });
     });
