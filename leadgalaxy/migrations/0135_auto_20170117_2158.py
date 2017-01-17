48ba98d668396e85a7dbd3c079fbd450cb13bb7c
Marian Gheata <victor960035@gmail.com>
2017-01-17 22:02:34 +0000

    Migration fix

--- leadgalaxy/migrations/0135_auto_20170117_2158.py
+++ leadgalaxy/migrations/0135_auto_20170117_2158.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('leadgalaxy', '0133_descriptiontemplate'),
+        ('leadgalaxy', '0134_shopifyordertrack_source_status_details'),
     ]
 
     operations = [
@@ -15,9 +15,8 @@ class Migration(migrations.Migration):
             name='ClippingMagicPlan',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('allowed_credits', models.BigIntegerField(default=0)),
-                ('amount', models.BigIntegerField(default=0)),
-                ('default', models.IntegerField(default=0)),
+                ('allowed_credits', models.IntegerField(default=0)),
+                ('amount', models.IntegerField(default=0, verbose_name=b'In USD')),
             ],
         ),
         migrations.RemoveField(
@@ -26,21 +25,19 @@ class Migration(migrations.Migration):
         ),
         migrations.RemoveField(
             model_name='clippingmagic',
-            name='downloaded_images',
+            name='api_id',
         ),
-        migrations.AddField(
+        migrations.RemoveField(
             model_name='clippingmagic',
-            name='allowed_credits',
-            field=models.BigIntegerField(default=-1),
+            name='api_secret',
+        ),
+        migrations.RemoveField(
+            model_name='clippingmagic',
+            name='downloaded_images',
         ),
         migrations.AddField(
             model_name='clippingmagic',
             name='remaining_credits',
             field=models.BigIntegerField(default=0),
-        ),
-        migrations.AddField(
-            model_name='clippingmagic',
-            name='clippingmagic_plan',
-            field=models.ForeignKey(related_name='clippingmagic_plan', blank=True, to='leadgalaxy.ClippingMagicPlan', null=True),
         ),
     ]
