323da9bc686810f31be540bce7dc1de9a07d61e1
Marian Gheata <victor960035@gmail.com>
2017-03-05 17:14:10 +0000

    Index Orders model fields

--- shopify_orders/models.py
+++ shopify_orders/models.py
@@ -78,8 +78,8 @@ class ShopifyOrder(models.Model):
     customer_name = models.CharField(max_length=256, blank=True, null=True, default='')
     customer_email = models.CharField(max_length=256, blank=True, null=True, default='')
 
-    financial_status = models.CharField(max_length=32, blank=True, null=True, default='')
-    fulfillment_status = models.CharField(max_length=32, blank=True, null=True, default='')
+    financial_status = models.CharField(max_length=32, blank=True, null=True, default='', db_index=True)
+    fulfillment_status = models.CharField(max_length=32, blank=True, null=True, default='', db_index=True)
 
     note = models.TextField(blank=True, null=True, default='')
     tags = models.TextField(blank=True, null=True, default='')
@@ -90,8 +90,8 @@ class ShopifyOrder(models.Model):
 
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    closed_at = models.DateTimeField(null=True, blank=True)
-    cancelled_at = models.DateTimeField(null=True, blank=True)
+    closed_at = models.DateTimeField(null=True, blank=True, db_index=True)
+    cancelled_at = models.DateTimeField(null=True, blank=True, db_index=True)
 
     def __unicode__(self):
         return u'#{} | {}'.format(self.order_number + 1000, self.store.title)
