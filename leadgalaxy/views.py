83a154838916cc3d921bbedad95676d674f6ab3d
Marian Gheata <victor960035@gmail.com>
2017-03-07 14:21:52 +0000

    Improve product syncing with Shopify

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -3351,11 +3351,13 @@ def product_view(request, pid):
             p['product']['description'] = shopify_product['body_html']
             p['product']['published'] = shopify_product['published_at'] is not None
 
-            if arrow.get(shopify_product['updated_at']).datetime > p['qelem'].updated_at and not settings.DEBUG:
-                messages.info(request, 'Product syncing with Shopify in progress...')
-                tasks.update_shopify_product.apply_async(
-                    args=[product.store.id, product.shopify_id],
-                    kwarg={'shopify_product': shopify_product, 'product_id': p['qelem'].id})
+            if arrow.get(shopify_product['updated_at']).datetime > p['qelem'].updated_at or request.GET.get('sync'):
+                print 'Syncing'
+                tasks.update_shopify_product(
+                    product.store.id,
+                    product.shopify_id,
+                    shopify_product=shopify_product,
+                    product_id=p['qelem'].id)
 
     breadcrumbs = [{'title': 'Products', 'url': '/product'}]
 
