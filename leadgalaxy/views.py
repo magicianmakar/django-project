1ad732ae6ef4a1f53f6d07684400e58ef446a266
Marian Gheata <victor960035@gmail.com>
2017-03-10 15:41:44 +0000

    Shopify: Fix for incomplete product links

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -2737,8 +2737,12 @@ def orders_place(request):
         for k in request.GET.keys():
             if k == 'SAPlaceOrder':
                 pass
+
             elif k == 'product':
-                event_data['product'] = re.findall('[/_]([0-9]+).html', request.GET[k])[0]
+                event_data['product'] = re.findall('[/_]([0-9]+).html', request.GET[k])
+                if event_data['product']:
+                    event_data['product'] = event_data['product'][0]
+
             elif k.startswith('SA'):
                 event_data[k[2:].lower()] = request.GET[k]
 
