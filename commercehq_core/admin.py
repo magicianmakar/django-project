952d359853ee902332f88d8e995f31e7eaa2f7cd
Marian Gheata <victor960035@gmail.com>
2017-03-11 17:50:57 +0000

    CHQ: Update Admin

--- commercehq_core/admin.py
+++ commercehq_core/admin.py
@@ -1,18 +1,34 @@
 from django.contrib import admin
 
-from .models import *
+from .models import (
+    CommerceHQStore,
+    CommerceHQProduct,
+    CommerceHQSupplier,
+    CommerceHQBoard,
+    CommerceHQOrderTrack,
+)
 
 
 @admin.register(CommerceHQStore)
 class CommerceHQStoreAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = ('user',)
 
 
 @admin.register(CommerceHQProduct)
 class CommerceHQProductAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = ('store', 'user', 'parent_product', 'default_supplier')
+
+
+@admin.register(CommerceHQSupplier)
+class CommerceHQSupplierAdmin(admin.ModelAdmin):
+    raw_id_fields = ('store', 'product')
 
 
 @admin.register(CommerceHQBoard)
 class CommerceHQBoardAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = ('user', 'products')
+
+
+@admin.register(CommerceHQOrderTrack)
+class CommerceHQOrderTrackAdmin(admin.ModelAdmin):
+    raw_id_fields = ('store', 'user')
