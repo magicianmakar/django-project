c1c6a886b70cdff51fa7336b350b283008fd887a
Marian Gheata <victor960035@gmail.com>
2017-03-13 13:24:58 +0000

    Fix

--- leadgalaxy/api.py
+++ leadgalaxy/api.py
@@ -2169,11 +2169,11 @@ class ShopifyStoreApi(ApiResponseMixin, View):
             permissions.user_can_view(user, product)
 
         except ShopifyProduct.DoesNotExist:
-            return self.api_error('Product not found', staus=404)
+            return self.api_error('Product not found', status=404)
 
         images = json.loads(product.data).get('images')
         if not images:
-            return self.api_error('Product doesn\'t have any images', staus=422)
+            return self.api_error('Product doesn\'t have any images', status=422)
 
         tasks.create_image_zip.delay(images, product.id)
 
