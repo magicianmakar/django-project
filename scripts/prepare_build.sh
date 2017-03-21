c2ef637235cedb2219aaacd288d3893cf746777a
Marian Gheata <victor960035@gmail.com>
2017-03-21 14:00:59 +0000

    CircelCI config file

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export DATABASE_URL="postgres://postgres:@postgres:5432/default-db"
-export DATA_STORE_DATABASE_URL="postgres://postgres:@postgres:5432/store-db"
+if [ -z "$DATABASE_URL" ]; then
+    export DATABASE_URL="postgres://postgres:@postgres:5432/default-db"
+fi
+
+if [ -z "$DATA_STORE_DATABASE_URL" ]; then
+    export DATA_STORE_DATABASE_URL="postgres://postgres:@postgres:5432/store-db"
+fi
+
+if [ -z "$REDISCLOUD_URL" ]; then
+    export REDISCLOUD_URL="redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}"
+    export REDISCLOUD_CACHE="$REDISCLOUD_URL"
+fi
+
 export DEBUG_APP="TRUE"
 export SENDGRID_PASSWORD=""
 export SENDGRID_USERNAME=""
@@ -9,8 +20,6 @@ export S3_BUCKET_NAME='shopifiedapp-assets'
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
 export JVZOO_SECRET=""
-export REDISCLOUD_URL="redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}"
-export REDISCLOUD_CACHE="redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}"
 export STRIPE_PUBLIC_KEY=""
 export STRIPE_SECRET_KEY=""
 export SHOPIFY_API_KEY=""
