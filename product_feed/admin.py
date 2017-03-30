5e841ad1ffce9a6ac3a45867f79e1aba5b5933da
Marian Gheata <victor960035@gmail.com>
2017-03-30 15:56:15 +0000

    Update Feed Status Admin

--- product_feed/admin.py
+++ product_feed/admin.py
@@ -7,4 +7,5 @@ from .models import FeedStatus
 class FeedStatusAdmin(admin.ModelAdmin):
     list_display = ('store', 'status', 'generation_time', 'fb_access_at', 'updated_at')
     raw_id_fields = ('store',)
-    exclude = []
+    list_filter = ('status',)
+    search_fields = ('store__id', 'store__title', 'store__user__id', 'store__user__email')
