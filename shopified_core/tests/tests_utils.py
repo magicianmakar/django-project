bf730885eed851f6c504d0f46088054aacc748ed
Marian Gheata <victor960035@gmail.com>
2017-04-25 19:47:48 +0000

    Test case for the default phone number

--- shopified_core/tests/tests_utils.py
+++ shopified_core/tests/tests_utils.py
@@ -17,67 +17,72 @@ from shopified_core.shipping_helper import (
 
 class UtilsTestCase(TestCase):
     def setUp(self):
-        pass
+        self.request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.70.0'})
 
     def test_order_phone_number(self):
         user = Mock(get_config=Mock(return_value='customer'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.70.0'})
 
-        country_code, phone_number = order_phone_number(request, user, '0652413300', 'FR')
+        country_code, phone_number = order_phone_number(self.request, user, '0652413300', 'FR')
         self.assertEqual(country_code, '+33')
         self.assertEqual(phone_number, '652413300')
 
-        country_code, phone_number = order_phone_number(request, user, '19716454717', 'ES')
+        country_code, phone_number = order_phone_number(self.request, user, '19716454717', 'ES')
         self.assertEqual(country_code, '+34')
         self.assertEqual(phone_number, '19716454717')
 
+    def test_order_customer_phone_number(self):
+        def get_config(name):
+            conf = {'order_default_phone': 'customer', 'order_phone_number': '9716454717'}
+            return conf.get(name)
+
+        user = Mock(get_config=get_config, profile=Mock(country='ES'))
+
+        country_code, phone_number = order_phone_number(self.request, user, '', 'FR')
+        self.assertEqual(country_code, '+34')
+        self.assertEqual(phone_number, '9716454717')
+
     def test_order_phone_number_pad(self):
         user = Mock(get_config=Mock(return_value='customer'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.70.0'})
 
-        country_code, phone_number = order_phone_number(request, user, '0033652413300', 'FR')
+        country_code, phone_number = order_phone_number(self.request, user, '0033652413300', 'FR')
         self.assertEqual(country_code, '+33')
         self.assertEqual(phone_number, '652413300')
 
-        country_code, phone_number = order_phone_number(request, user, '+33652413300', 'FR')
+        country_code, phone_number = order_phone_number(self.request, user, '+33652413300', 'FR')
         self.assertEqual(country_code, '+33')
         self.assertEqual(phone_number, '652413300')
 
-        country_code, phone_number = order_phone_number(request, user, '33652413300', 'FR')
+        country_code, phone_number = order_phone_number(self.request, user, '33652413300', 'FR')
         self.assertEqual(country_code, '+33')
         self.assertEqual(phone_number, '652413300')
 
     def test_empty_order_phone_numbers(self):
         user = Mock(get_config=Mock(return_value='000000000000'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.62.0'})
 
-        country_code, phone_number = order_phone_number(request, user, '0652413300', 'US')
+        country_code, phone_number = order_phone_number(self.request, user, '0652413300', 'US')
         self.assertEqual(country_code, '+1')
         self.assertEqual(phone_number, '000000000000')
 
         user = Mock(get_config=Mock(return_value='000-0000'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.62.0'})
 
-        country_code, phone_number = order_phone_number(request, user, '0652413300', 'US')
+        country_code, phone_number = order_phone_number(self.request, user, '0652413300', 'US')
         self.assertEqual(country_code, '+1')
         self.assertEqual(phone_number, '0000000')
 
         user = Mock(get_config=Mock(return_value='000-0000'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.62.0'})
 
-        country_code, phone_number = order_phone_number(request, user, '0652413300', 'AU')
+        country_code, phone_number = order_phone_number(self.request, user, '0652413300', 'AU')
         self.assertEqual(country_code, '+61')
         self.assertEqual(phone_number, '0000000')
 
-        country_code, phone_number = order_phone_number(request, user, '0652413300', '')
+        country_code, phone_number = order_phone_number(self.request, user, '0652413300', '')
         self.assertEqual(country_code, '+1')
         self.assertEqual(phone_number, '0000000')
 
     def test_null_order_phone_numbers(self):
         user = Mock(get_config=Mock(return_value='customer'))
-        request = Mock(META={'HTTP_X_EXTENSION_VERSION': '1.62.0'})
 
-        country_code, phone_number = order_phone_number(request, user, None, 'US')
+        country_code, phone_number = order_phone_number(self.request, user, None, 'US')
         self.assertEqual(country_code, '+1')
         self.assertEqual(phone_number, '')
 
