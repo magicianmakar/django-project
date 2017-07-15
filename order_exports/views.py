b2572286f27932929022ac9ecb6e05bd6158542b
Marian Gheata <victor960035@gmail.com>
2017-07-15 16:04:07 +0000

    UI Improvements

--- order_exports/views.py
+++ order_exports/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
+import arrow
 import simplejson as json
-from datetime import datetime
 
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect, get_object_or_404
