d5ec1353f15f287d29d6a29d4b9d13d83c9515c2
Marian Gheata <victor960035@gmail.com>
2017-08-22 12:33:59 +0000

    Validate Custom Tags

--- leadgalaxy/api.py
+++ leadgalaxy/api.py
@@ -1379,6 +1379,14 @@ class ShopifyStoreApi(ApiResponseMixin, View):
         for key in bool_config:
             config[key] = (key in data)
 
+        if config.get('aliexpress_order_tags'):
+            if re.findall('https?://', config.get('aliexpress_order_tags')):
+                return self.api_error('Invalid Custom Tag - Order Placed', status=422)
+
+        if config.get('tracking_number_tags'):
+            if re.findall('https?://', config.get('tracking_number_tags')):
+                return self.api_error('Invalid Custom Tag - Tracking Number Added', status=422)
+
         profile.config = json.dumps(config)
         profile.save()
 
