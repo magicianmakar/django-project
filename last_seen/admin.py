86b4e5fbbe1e0bcbdfb2701f5a4a270981d67ad7
Marian Gheata <victor960035@gmail.com>
2017-09-16 21:56:47 +0000

    Add Plan to last seen Admin

--- last_seen/admin.py
+++ last_seen/admin.py
@@ -5,9 +5,12 @@ from models import LastSeen
 
 
 class LastSeenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'module', 'last_seen')
-    list_filter = ('module', 'last_seen')
+    list_display = ('user', 'plan', 'module', 'last_seen')
+    list_filter = ('module', 'last_seen', 'user__profile__plan')
     search_fields = ('user__username', 'user__email')
+
+    def plan(self, obj):
+        return obj.user.profile.plan.title
 
 
 admin.site.register(LastSeen, LastSeenAdmin)
