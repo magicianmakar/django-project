b8be32f35034e3f907e73e9bf4d60b2a02e2815e
Ahmed Saidi <boussouira@users.noreply.github.com>
2017-09-16 15:33:00 +0000

    Detect missing migrations in CI

--- scripts/common_issues.sh
+++ scripts/common_issues.sh
@@ -7,3 +7,11 @@ if [ "$?" == "0" ]; then
     echo "[-] Do not use 'datetime.strptime', use arrow or django.utils.timezone for timezone aware datetime:"
     exit -1
 fi
+
+python manage.py makemigrations | grep 'No changes detected'
+
+if [ ! "$?" == "0" ]; then
+    echo
+    echo "[-] Your models have changes that are not yet reflected in a migration, run: manage.py makemigrations"
+    exit -2
+fi
