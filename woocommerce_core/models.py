5f487255ad61e07b447462dd16a0d2d6d7831a1b
Marian Gheata <victor960035@gmail.com>
2017-10-25 23:28:32 +0000

    Fix WooCommerce API calls

--- woocommerce_core/models.py
+++ woocommerce_core/models.py
@@ -61,10 +61,11 @@ class WooStore(models.Model):
             url=self.api_url,
             consumer_key=self.api_key,
             consumer_secret=self.api_password,
-            wp_api=True,
-            version='wc/v2',
+            # wp_api=True,
+            # version='wc/v2',
             verify_ssl=False,
-            timeout=300)
+            query_string_auth=True,
+            timeout=15)
 
     def save(self, *args, **kwargs):
         if not self.store_hash:
