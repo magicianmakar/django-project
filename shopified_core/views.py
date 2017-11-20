3b3a50108c308c0545ef8e8ec2bcb5555a684e8d
Marian Gheata <victor960035@gmail.com>
2017-11-20 22:17:26 +0000

    Misc: Check if valid timestamp

--- shopified_core/views.py
+++ shopified_core/views.py
@@ -211,7 +211,7 @@ class ShopifiedApi(ApiResponseMixin, View):
         since_key = 'sync_since_{}'.format(user.id)
         all_orders = cache.get(since_key) is None
 
-        if data.get('since') and not all_orders:
+        if core_utils.safeInt(data.get('since')) and not all_orders:
             since = arrow.get(data.get('since')).datetime
         else:
             since = arrow.now().replace(days=-30).datetime
