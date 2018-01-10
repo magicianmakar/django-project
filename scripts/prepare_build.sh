ffdd78c3bd75709a0e92d4d7c301a98301cb1ec2
Ahmed Saidi <boussouira@users.noreply.github.com>
2018-01-10 22:09:04 +0000

    Unused Environment Variables (#116)

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-if [ -z "$DATABASE_URL" ]; then
-    export DATABASE_URL="postgres://postgres:@postgres:5432/default-db"
-fi
-
-if [ -z "$DATA_STORE_DATABASE_URL" ]; then
-    export DATA_STORE_DATABASE_URL="postgres://postgres:@postgres:5432/store-db"
-fi
-
-if [ -z "$REDISCLOUD_URL" ]; then
-    export REDISCLOUD_URL="redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}"
-fi
-
-export REDISCLOUD_CACHE="$REDISCLOUD_URL"
-
-export DEBUG_APP="TRUE"
-export SENDGRID_PASSWORD=""
-export SENDGRID_USERNAME=""
-export S3_BUCKET_NAME='shopifiedapp-assets'
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
-export JVZOO_SECRET=""
-export STRIPE_PUBLIC_KEY=""
-export STRIPE_SECRET_KEY=""
-export SHOPIFY_API_KEY=""
-export SHOPIFY_API_SECRET=""
-
-
 if [ ! -d "venv" ]; then
     virtualenv venv
 fi
