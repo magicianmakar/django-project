02de5d9eef3783441d2d389c034cdd5ad1b60b9c
Marian Gheata <victor960035@gmail.com>
2018-01-31 23:40:06 +0000

    Improve monitor products command

--- leadgalaxy/management/commands/monitor_products.py
+++ leadgalaxy/management/commands/monitor_products.py
@@ -9,6 +9,7 @@ from shopified_core.management import DropifiedBaseCommand
 from leadgalaxy.models import *
 from commercehq_core.models import CommerceHQProduct
 from product_alerts.utils import monitor_product
+from last_seen.models import LastSeen
 
 
 PRICE_MONITOR_BASE = '{}/api'.format(settings.PRICE_MONITOR_HOSTNAME)
@@ -29,21 +30,14 @@ class Command(DropifiedBaseCommand):
     help = 'Add products to be monitored'
 
     def add_arguments(self, parser):
-        parser.add_argument('action', nargs=1, type=str,
-                            choices=['attach', 'detach'], help='Action')
-        parser.add_argument('store_type', nargs=1, type=str,
-                            choices=['shopify', 'chq'], help='Store Type')
-
         parser.add_argument('--plan', dest='plan_id', action='append', type=int, help='Plan ID')
         parser.add_argument('--exclude-plan', dest='exclude_plan_id', action='append', type=int, help='Plan ID')
         parser.add_argument('--user', dest='user_id', action='append', type=int, help='User ID')
         parser.add_argument('--permission', dest='permission', action='append', type=str, help='Users with permission')
+        parser.add_argument('--remove-inactive', dest='remove-inactive', action='store_true', help='Remove products of inactive users')
 
     def start_command(self, *args, **options):
         self.ignored_users = [16088, 10052, 10889, 13366, 8869, 6680, 8699, 11765, 14651, 12755, 10142, 14970]
-
-        action = options['action'][0]
-        store_type = options['store_type'][0]
 
         if not options['plan_id']:
             options['plan_id'] = []
@@ -66,14 +60,10 @@ class Command(DropifiedBaseCommand):
         for plan_id in options['plan_id']:
             try:
                 plan = GroupPlan.objects.get(pk=plan_id)
-                self.stdout.write(self.style.MIGRATE_SUCCESS(
-                    '{} webhooks for Plan: {}'.format(action.title(), plan.title)))
+                self.stdout.write(self.style.MIGRATE_SUCCESS('Plan: {}'.format(plan.title)))
 
                 for profile in plan.userprofile_set.select_related('user').all():
-                    if store_type == 'shopify':
-                        self.handle_products(profile.user, profile.user.shopifyproduct_set.all(), action, options)
-                    if store_type == 'chq':
-                        self.handle_products(profile.user, profile.user.commercehqproduct_set.all(), action, options)
+                    self.handle_user(profile.user, options)
 
             except GroupPlan.DoesNotExist:
                 raise CommandError('Plan "%s" does not exist' % plan_id)
@@ -85,40 +75,50 @@ class Command(DropifiedBaseCommand):
                         self.stdout.write('* Ignore Plan: {}'.format(plan.title))
                         continue
 
-                    self.stdout.write(self.style.MIGRATE_SUCCESS(
-                        '{} webhooks for Plan: {}'.format(action.title(), plan.title)))
+                    self.stdout.write(self.style.MIGRATE_SUCCESS(u'Plan: {}'.format(plan.title)))
 
                     for profile in plan.userprofile_set.select_related('user').all():
-                        if store_type == 'shopify':
-                            self.handle_products(profile.user, profile.user.shopifyproduct_set.all(), action, options)
-                        if store_type == 'chq':
-                            self.handle_products(profile.user, profile.user.commercehqproduct_set.all(), action, options)
+                        self.handle_user(profile.user, options)
 
                 for bundle in p.featurebundle_set.all():
-                    self.stdout.write(self.style.MIGRATE_SUCCESS(
-                        '{} webhooks for Bundle: {}'.format(action.title(), bundle.title)))
+                    self.stdout.write(self.style.MIGRATE_SUCCESS(u'Bundle: {}'.format(bundle.title)))
+
                     for profile in bundle.userprofile_set.select_related('user').all():
-                        if store_type == 'shopify':
-                            self.handle_products(profile.user, profile.user.shopifyproduct_set.all(), action, options)
-                        if store_type == 'chq':
-                            self.handle_products(profile.user, profile.user.commercehqproduct_set.all(), action, options)
+                        self.handle_user(profile.user, options)
 
         for user_id in options['user_id']:
             try:
                 user = User.objects.get(pk=user_id)
-                products = None
-                if store_type == 'shopify':
-                    products = ShopifyProduct.objects.filter(user=user)
-                if store_type == 'chq':
-                    products = CommerceHQProduct.objects.filter(user=user)
-                self.handle_products(user, products, action, options)
+                self.handle_user(user, options)
 
             except User.DoesNotExist:
                 raise CommandError('User "%s" does not exist' % user_id)
 
         self.q.join()
 
-    def handle_products(self, user, products, action, options):
+    def handle_user(self, user, options):
+        try:
+            LastSeen.objects.when(user, 'website')
+        except LastSeen.DoesNotExist:
+            self.stdout.write(u'Ignore inactive user: {}'.format(user.username))
+            if options['remove-inactive']:
+                rep = requests.delete(
+                    url='{}/products'.format(PRICE_MONITOR_BASE),
+                    params={'user': user.id},
+                    auth=(settings.PRICE_MONITOR_USERNAME, settings.PRICE_MONITOR_PASSWORD)
+                )
+
+                rep.raise_for_status()
+
+                if not rep.text.startswith('0 '):
+                    self.write(u'\t{}'.format(rep.text))
+
+            return
+
+        self.handle_products(user, CommerceHQProduct.objects.filter(user=user))
+        self.handle_products(user, ShopifyProduct.objects.filter(user=user))
+
+    def handle_products(self, user, products):
         products = products.filter(Q(monitor_id=0) | Q(monitor_id=None))
 
         if products.model.__name__ == 'ShopifyProduct':
@@ -139,15 +139,14 @@ class Command(DropifiedBaseCommand):
                 self.stdout.write(u'Too many products ({}) for user: {}'.format(products_count, user.username))
                 return
 
-            self.stdout.write(u'{} webhooks to {} product for user: {}'.format(
-                action.title(), products_count, user.username), self.style.HTTP_INFO)
+            self.stdout.write(u'Add {} products for user: {}'.format(products_count, user.username), self.style.HTTP_INFO)
 
             for product in products:
-                self.handle_product(product, action)
+                self.handle_product(product)
 
         self.q.join()
 
-    def handle_product(self, product, action):
+    def handle_product(self, product):
         if product.monitor_id:
             self.stdout.write(self.style.HTTP_INFO('Ignore, already registered.'))
             return
