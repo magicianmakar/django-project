14b377262649f3cf0c0aec750d1949d342923ab3
Marian Gheata <victor960035@gmail.com>
2018-02-08 19:30:52 +0000

    Fix: Type casting

--- stripe_subscription/utils.py
+++ stripe_subscription/utils.py
@@ -497,9 +497,9 @@ def process_webhook_event(request, event_id, raven_client):
         except User.DoesNotExist:
             description = safeStr(charge.description)
 
-            is_unlimited = request.POST.get('unlimited')
-            is_unlimited |= 'Unlimited' in description and ('ECOM Jam' in description or '$997' in description)
-            is_unlimited |= charge.metadata.get('products') == '$997 Lifetime Offer'
+            is_unlimited = bool(request.POST.get('unlimited'))
+            is_unlimited |= bool('Unlimited' in description and ('ECOM Jam' in description or '$997' in description))
+            is_unlimited |= bool(charge.metadata.get('products') == '$997 Lifetime Offer')
 
             if is_unlimited:
                 stripe_customer = stripe.Customer.retrieve(charge.customer)
