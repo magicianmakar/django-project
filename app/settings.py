2d9dee204b528533f0c60d8680d79a124ed5dd67
Marian Gheata <victor960035@gmail.com>
2018-05-03 10:45:35 +0000

    Add inventory to Shopify OAuth scopes

--- app/settings.py
+++ app/settings.py
@@ -225,7 +225,8 @@ SHOPIFY_API_KEY = os.environ['SHOPIFY_API_KEY']
 SHOPIFY_API_SECRET = os.environ['SHOPIFY_API_SECRET']
 SHOPIFY_API_SCOPE = u','.join([
     'write_apps', 'write_content', 'write_products', 'write_customers',
-    'write_orders', 'write_fulfillments', 'write_shipping', 'read_analytics'
+    'write_orders', 'write_fulfillments', 'write_shipping', 'read_analytics',
+    'write_inventory'
 ])
 
 # Slack API
