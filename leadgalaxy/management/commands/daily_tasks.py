3eafa685e4a2a9ec6dd5728a44a14bb5de3d39ed
Marian Gheata <victor960035@gmail.com>
2018-07-15 20:26:26 +0000

    Optimize Alerts deletion

--- leadgalaxy/management/commands/daily_tasks.py
+++ leadgalaxy/management/commands/daily_tasks.py
@@ -5,18 +5,11 @@ import arrow
 from leadgalaxy.models import *
 from product_alerts.models import ProductChange
 from shopified_core.management import DropifiedBaseCommand
+from stripe_subscription.utils import invoice_extra_stores
 
 
 class Command(DropifiedBaseCommand):
     def start_command(self, *args, **options):
-        # Archive alerts after 7 days
-        # archive_date = arrow.utcnow().replace(days=-7).datetime
-        # ProductChange.objects.filter(hidden=False, created_at__lt=archive_date).update(hidden=True)
-
-        # Remove alerts after 30 days
-        delete_date = arrow.utcnow().replace(days=-30).datetime
-        ProductChange.objects.filter(created_at__lt=delete_date).delete()
-
         # Expired plans
         self.stdout.write('Change plan of expired profiles', self.style.HTTP_INFO)
         for profile in UserProfile.objects.filter(plan_expire_at__lte=timezone.now()):
@@ -30,8 +23,11 @@ class Command(DropifiedBaseCommand):
                 profile.save()
 
         # Invoice Extra Stores
-        from stripe_subscription.utils import invoice_extra_stores
+        self.stdout.write('Inoice Extra Stores', self.style.HTTP_INFO)
         invoice_extra_stores()
+
+        self.stdout.write('Delete Old Alerts', self.style.HTTP_INFO)
+        self.delete_alerts()
 
     def profile_changed(self, profile, expired_plan, new_plan):
         data = {
@@ -47,3 +43,19 @@ class Command(DropifiedBaseCommand):
             subject='[Dropified] Plan Expire',
             recipient=['ma7dev@gmail.com', 'chase@dropified.com'],
             data=data)
+
+    def delete_alerts(self):
+        # Remove alerts after 30 days
+        delete_date = arrow.utcnow().replace(days=-30).datetime
+        alert_ids = list(ProductChange.objects.filter(created_at__lt=delete_date).values_list('id', flat=True))
+        alerts_count = len(alert_ids)
+
+        self.stdout.write('Delete {} Alerts'.format(alerts_count))
+
+        steps = 10000
+        start = 0
+        while start < alerts_count:
+            order_ids = alert_ids[start:start + steps]
+            ProductChange.objects.filter(id__in=order_ids).delete()
+
+            start += steps
