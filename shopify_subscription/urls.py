57e436931e78920aea32d5c603fff27e9bc0479f
Marian Gheata <victor960035@gmail.com>
2018-08-18 01:28:41 +0000

    Missing Url name

--- shopify_subscription/urls.py
+++ shopify_subscription/urls.py
@@ -4,6 +4,6 @@ import shopify_subscription.views
 
 urlpatterns = [
     url(r'^plan$', shopify_subscription.views.subscription_plan, name='shopify_subscription.views.subscription_plan'),
-    url(r'^activated$', shopify_subscription.views.subscription_activated),
-    url(r'^charged/(?P<store>[0-9]+)$', shopify_subscription.views.subscription_charged),
+    url(r'^activated$', shopify_subscription.views.subscription_activated, name='shopify_subscription.views.subscription_activated'),
+    url(r'^charged/(?P<store>[0-9]+)$', shopify_subscription.views.subscription_charged, name='shopify_subscription.views.subscription_charged'),
 ]
