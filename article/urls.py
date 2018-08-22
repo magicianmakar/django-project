56bc5095b64cfe0b065d60c666a1aeb206a4ac4f
Marian Gheata <victor960035@gmail.com>
2018-08-22 11:21:38 +0000

    Url name fix

--- article/urls.py
+++ article/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 import article.views
 
 urlpatterns = [
-    url(r'^$', article.views.index),
+    url(r'^$', article.views.index, name='article.views.index'),
     url(r'^tagged/(?P<tag>[a-z-]+)$$', article.views.index, name="tagged"),
-    url(r'^view/(?P<id_article>\d+)$', article.views.view),
-    url(r'^view/(?P<slug_article>[a-zA-Z0-9-_]+)$', article.views.view),
+    url(r'^view/(?P<id_article>\d+)$', article.views.view, name='article.views.view'),
+    url(r'^view/(?P<slug_article>[a-zA-Z0-9-_]+)$', article.views.view, name='article.views.view'),
     url(r'^edit/(?P<article_id>\d+)$', article.views.edit, name="edit"),
     url(r'^add$', article.views.submit, name="add-page"),
-    url(r'^(?P<id_article>\d+)$', article.views.view),
-    url(r'^(?P<slug_article>[a-zA-Z0-9-_]+)$', article.views.view),
+    url(r'^(?P<id_article>\d+)$', article.views.view, name='article.views.view'),
+    url(r'^(?P<slug_article>[a-zA-Z0-9-_]+)$', article.views.view, name='article.views.view'),
 ]
