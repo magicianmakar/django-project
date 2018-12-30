080b02fac9b83d986b308aca2a171bbb32e60f5e
Marian Gheata <victor960035@gmail.com>
2018-12-30 16:56:14 +0000

    Alerts: Send product change alerts based on the user Timezone
    Send Alerts emails in batch for user with local time close 9AM

--- product_alerts/management/commands/notify_product_changes.py
+++ product_alerts/management/commands/notify_product_changes.py
@@ -1,4 +1,5 @@
 import arrow
+import pytz
 
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -17,41 +18,62 @@ class Command(DropifiedBaseCommand):
 
     users_more_changes = {}
 
+    def get_users(self):
+        """ Return users that will be notified
+
+        Find users where time is (arround) 9:00 AM using the user's selected TimeZone in Profile page
+        """
+        users = []
+        tz_list = []
+        for tz in pytz.all_timezones_set:
+            if arrow.now().to(tz).hour == 9:
+                tz_list.append(tz)
+
+                users.extend(User.objects.filter(profile__timezone=tz, profile__subuser_parent=None))
+
+        self.write('Notfiy {} Users in {} Timezones'.format(len(users), len(tz_list)))
+
+        return users
+
     def start_command(self, *args, **options):
-        prodduct_changes = ProductChange.objects.filter(notified_at=None, created_at__range=[i.datetime for i in arrow.utcnow().span('day')]) \
-                                                .order_by('user_id')
+        users = self.get_users()
 
-        changes_user_map = {}
-        changes_count = 0
-        for change in prodduct_changes[:10000]:
-            if change.user_id not in changes_user_map:
-                changes_user_map[change.user_id] = []
+        if not len(users):
+            self.write('No user found to notify')
+            return
 
-            if len(changes_user_map[change.user_id]) <= 20:
-                changes_user_map[change.user_id].append(change)
-            else:
-                self.users_more_changes[change.user_id] = self.users_more_changes.get(change.user_id, 0) + 1
-
-            changes_count += 1
-
-        self.stdout.write('Notfiy {} changes for {} users'.format(changes_count, len(changes_user_map)))
-        self.stdout.write('Notfiy {}/{} extra changes'.format(len(self.users_more_changes), sum(self.users_more_changes.values())))
+        self.write('Notfiy changes for {} users'.format(len(users)))
 
         ignored_users = set()
-        for user_id, changes in changes_user_map.items():
+        for user in users:
+            prodduct_changes = ProductChange.objects.filter(user=user, notified_at=None, seen=False)
+
+            last_notified_key = 'last_alert_id_{}'.format(user.id)
+            last_notified_id = cache.get(last_notified_key, 30860000)
+
+            prodduct_changes = prodduct_changes.filter(id__gt=last_notified_id)
+
+            changes_list = list(prodduct_changes.order_by('-created_at')[:100])
+            changes = changes_list[:20]
+
+            if not changes:
+                continue
+
+            self.write('Notfiy {} about {} changes from {}'.format(user.email, len(changes_list), last_notified_id))
+
+            notified_key = 'change_alert_{}'.format(user.id)
+            if cache.get(notified_key):
+                ignored_users.add(user)
+                continue
+
+            cache.set(last_notified_key, changes[0].id, timeout=86400 * 3)
+
             try:
-                user = User.objects.get(pk=user_id)
+                self.handle_changes(user, changes, len(changes_list))
 
-                notified_key = 'change_alert_{}'.format(user.id)
-                if cache.get(notified_key):
-                    ignored_users.add(user)
-                    continue
+                cache.set(notified_key, True, timeout=43200)
 
-                self.handle_changes(user, changes)
-
-                cache.set(notified_key, True, timeout=86400)
-
-                ProductChange.objects.filter(id__in=[j.id for j in changes]).update(notified_at=timezone.now())
+                ProductChange.objects.filter(id__in=[j.id for j in changes_list]).update(notified_at=timezone.now())
 
             except User.DoesNotExist:
                 raven_client.captureException(level='warning')
@@ -60,9 +82,9 @@ class Command(DropifiedBaseCommand):
                 raven_client.captureException()
 
         if len(ignored_users):
-            self.stdout.write('Notfiy {} ignored users'.format(len(ignored_users)))
+            self.write('Notfiy {} ignored users'.format(len(ignored_users)))
 
-    def handle_changes(self, user, changes):
+    def handle_changes(self, user, changes, changes_count):
         user_changes_map = {
             'availability': [],
             'price': [],
@@ -84,15 +106,15 @@ class Command(DropifiedBaseCommand):
                 if sub_user.profile.subuser_stores.filter(is_active=True).count():
                     cc_list.append(sub_user.email)
 
-        self.send_email(user, changes_map, cc_list=cc_list)
+        self.send_email(user, changes_map, cc_list=cc_list, changes_count=changes_count)
         return changes_map
 
-    def send_email(self, user, changes_map, cc_list=[]):
+    def send_email(self, user, changes_map, cc_list=[], changes_count=None):
         # send changes_map to email template
         data = {
             'username': user.username,
             'changes_map': changes_map,
-            'have_more_changes': self.users_more_changes.get(user)
+            'have_more_changes': changes_count,
         }
 
         if any([changes_map['availability'],
