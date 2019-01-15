12ef7ddd253d90474e26c89c99af3f46171dbddd
Dominic Yordan <dominicyordan@users.noreply.github.com>
2019-01-15 23:47:31 +0800

    Move Import Product Reviews to Webapp (DROP-893) (#357)
    * Adds UI to product page and fetches feedback page details
    
    * Download CSV of reviews
    
    * Show error message, improved error message, and added delay to the fetching of review details.
    
    * Add options to product reviews
    
    * Check if extension is installed
    
    * Send extension message
    
    * Send product URL to extension instead of product ID
    
    * Update review form details
    
    * Use one endpoint for product review details and product review imports.
    
    * Removed backend code for retrieving product reviews.
    
    * Fixed option labels
    
    * Form does not update when page number and sorting are changed.
    
    * Corrected wrong property name. Do not reload details when text only option is changed.
    
    * Misc: Handle null responses
    
    On older versions the extension will return null
    
    * Use Shopify Product Handle
    
    This is the main reason for moving the reviews outside of the extension
    So we get the exact handler the product is using in Shopify

---
+++ leadgalaxy/static/shopified/js/reviews.js
@@ -0,0 +1,151 @@
+(function() {
+    'use strict';
+
+    var $isExtensionReady = isExtensionReady();
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        var target = $(e.target).data("auto-click");
+        if (target == '#reviews') {
+            $isExtensionReady.done(updateReviewsForm);
+        }
+    });
+
+    if (window.location.hash) {
+        if (window.location.hash.substring(1) === 'reviews') {
+            $isExtensionReady.done(updateReviewsForm);
+        }
+    }
+
+    $('#review-form').submit(getReviews)
+                     .find('input, select')
+                     .not('#reviews_pages')
+                     .not('#reviews_sort')
+                     .not('#only_text_reviews')
+                     .on('change', updateReviewsForm);
+
+    function isExtensionReady() {
+        var $deferred = $.Deferred();
+
+        var timerId = setInterval(function() {
+            if (typeof window.extensionSendMessage === 'function') {
+                $deferred.resolve();
+                clearInterval(timerId);
+            }
+        }, 1000);
+
+        setTimeout(function() {
+            if (typeof window.extensionSendMessage !== 'function') {
+                $deferred.reject();
+                clearInterval(timerId);
+            }
+        }, 5000);
+
+        return $deferred.promise();
+    }
+
+    function updateReviewsForm() {
+        disableForm();
+        window.extensionSendMessage({
+            subject: 'getFeedbackPageDetails',
+            data: getPostData(),
+        }, function(response) {
+            enableForm();
+
+            if (response && response.ok) {
+                addDetails(response.details);
+            }
+        });
+    }
+
+    function getPostData() {
+        return {
+            product_url: $('#reviews_product_url').val(),
+            reviews_star: $('#reviews_star').val(),
+            reviews_sort: $('#reviews_sort').val(),
+            reviews_with_images: $('#reviews_with_images').prop('checked'),
+            reviews_personal_info: $('#reviews_personal_info').prop('checked'),
+            reviews_my_country: $('#reviews_my_country').prop('checked'),
+            only_text_reviews: $('#only_text_reviews').prop('checked'),
+            reviews_pages: $('#reviews_pages').val(),
+        };
+    }
+
+    function labelTotal(total) {
+        return total + ' ' + (total === 1 ? 'page' : 'pages');
+    }
+
+    function addDetails(data) {
+        $('label[for=reviews_pages]').text('Pages to import (Total: ' + labelTotal(data.review_pages) + '): ');
+        $('#reviews_pages').attr('max', data.review_pages);
+        $('label[for=reviews_with_images]').text('With pictures (' + labelTotal(data.with_pictures) + ')');
+        $('label[for=reviews_personal_info]').text('With personal information (' + labelTotal(data.with_personal_info) + ')');
+    }
+
+    function enableForm() {
+        $('#review-form').find('input, select, button').prop('disabled', false);
+    }
+
+    function disableForm() {
+        $('#review-form').find('input, select, button').prop('disabled', true);
+    }
+
+    function addDownloadButton(rows) {
+        var csv = 'product_handle,state,rating,title,author,email,location,body,reply,created_at,replied_at\n';
+
+        for (var i = 0, len = rows.length; i < len; i++) {
+            var row = rows[i];
+            csv += row.join();
+            csv += '\n';
+        }
+
+        $("<span>&nbsp;<a href=\"data:text/csv;charset=utf-8," +
+            encodeURI(csv) + "\" class='btn btn-info " +
+            "csv-reviews' id='csv_reviews')' target='_blank' download='reviews.csv'" +
+            "><i class='fa fa-download'></i> Download</a></span>"
+         ).insertAfter('#import-reviews-btn');
+    }
+
+    function getReviewRow(review) {
+        return [
+            $('#product-title').data('handle'),
+            'published',
+            review.stars,
+            review.title,
+            review.author,
+            'user@example.mail',
+            review.location,
+            "\"" + review.body + "\"",
+            "\"\"",
+            review.dateCreated,
+            "\"\"",
+        ];
+    }
+
+    function getReviewRows(reviews) {
+        var rows = [];
+
+        for (var i = 0, len = reviews.length; i < len; i++) {
+            var review = reviews[i];
+            var row = getReviewRow(review);
+            rows.push(row);
+        }
+
+        return rows;
+    }
+
+    function getReviews(e) {
+        e.preventDefault();
+        window.extensionSendMessage({
+            subject: 'getReviews',
+            data: getPostData(),
+        }, function(response) {
+            if (response && response.ok) {
+                var rows = getReviewRows(response.reviews);
+                addDownloadButton(rows);
+                disableForm();
+            } else {
+                enableForm();
+            }
+        });
+    }
+})();
