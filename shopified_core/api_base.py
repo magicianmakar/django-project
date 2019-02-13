7a268544a96cae600273a5398b851c08a582e0a0
Marian Gheata <victor960035@gmail.com>
2019-02-13 13:06:11 +0000

    Assert models are configured on initialization

--- shopified_core/api_base.py
+++ shopified_core/api_base.py
@@ -8,11 +8,17 @@ from shopified_core.mixins import ApiResponseMixin
 class ApiBase(ApiResponseMixin, View):
     board_model = None
 
+    def __init__(self):
+        super(ApiBase, self).__init__()
+        self.assert_configured()
+
+    def assert_configured(self):
+        assert self.board_model, 'Boards Model is not set'
+
     def post_board_favorite(self, request, user, data):
         if not user.can('edit_product_boards.sub'):
             raise PermissionDenied()
 
-        assert self.board_model is not None
         board = self.board_model.objects.get(id=data.get('board'))
         permissions.user_can_edit(user, board)
 
