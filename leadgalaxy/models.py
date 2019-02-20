60fd9986bd6b2e50df12d3a67659a776c4ef2321
Marian Gheata <victor960035@gmail.com>
2019-02-20 18:07:59 +0000

    Misc: Handle exception

--- leadgalaxy/models.py
+++ leadgalaxy/models.py
@@ -2410,9 +2410,12 @@ def update_plan_changed_date(sender, instance, created, **kwargs):
     user = instance.user
     current_plan = None
 
-    change_log, created = GroupPlanChangeLog.objects.get_or_create(user=user)
+    try:
+        change_log, created = GroupPlanChangeLog.objects.get_or_create(user=user)
+    except:
+        change_log = GroupPlanChangeLog.objects.filter(user=user).first()
 
-    if current_plan != change_log.plan:
+    if change_log and current_plan != change_log.plan:
         change_log.previous_plan = change_log.plan
         change_log.plan = current_plan
         change_log.changed_at = arrow.utcnow().datetime
