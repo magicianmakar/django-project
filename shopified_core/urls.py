d3e073f9c97049ea50afd929c4961813de50e3f3
Marian Gheata <victor960035@gmail.com>
2019-04-09 08:39:53 +0000

    API: Add dash to store type

--- shopified_core/urls.py
+++ shopified_core/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 import shopified_core.views
 
 urlpatterns = [
-    url(r'^api/(v(?P<version>[0-9])/)?((?P<store_type>[a-z]+)/)?(?P<target>[a-z_-]+)$',
+    url(r'^api/(v(?P<version>[0-9])/)?((?P<store_type>[a-z_-]+)/)?(?P<target>[a-z_-]+)$',
         shopified_core.views.ShopifiedApiBase.as_view()),
 ]
