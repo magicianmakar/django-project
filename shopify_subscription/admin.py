1105a33bc9535d0b1b75b07fdcdf62e17382fa2a
Marian Gheata <victor960035@gmail.com>
2019-07-06 11:28:40 +0000

    Misc: More admin filters in Shopify Subscription model

--- shopify_subscription/admin.py
+++ shopify_subscription/admin.py
@@ -10,7 +10,7 @@ class ShopifySubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'store', 'plan', 'subscription_id', 'status',
                     'data', 'activated_on', 'created_at', 'updated_at', )
 
-    list_filter = ('status',)
+    list_filter = ('charge_type', 'status', 'plan__payment_gateway', 'plan__payment_interval')
     raw_id_fields = ('user', 'store')
     readonly_fields = ('subscription_id', 'status', 'activated_on', 'created_at', 'updated_at',)
     search_fields = ('store__id', 'store__title', 'store__shop') + USER_SEARCH_FIELDS
