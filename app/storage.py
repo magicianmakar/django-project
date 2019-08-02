b6219adda64406667a3db0f948d85ad84cda1a08
Marian Gheata <victor960035@gmail.com>
2019-08-02 11:16:51 +0000

    Append file hash to static file urls (#661)
    Files without a hash in their url don't get invalidated from Cloudfront edges, unless we use compressor
    This change will append a hash to each JS and CSS on each git head change
    Closes #660

--- app/storage.py
+++ app/storage.py
@@ -1,24 +1,40 @@
-from django.core.files.storage import get_storage_class
-from storages.backends.s3boto import S3BotoStorage
+from hashlib import sha1
+import os
+
 from django.conf import settings
+
+from storages.backends.s3boto import S3BotoStorage
+from compressor.storage import CompressorFileStorage
 
 
 class CachedS3BotoStorage(S3BotoStorage):
     """
     S3 storage backend that saves the files locally, too.
     """
+
     def __init__(self, *args, **kwargs):
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.location = settings.STATICFILES_LOCATION
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
+        self.local_storage = CompressorFileStorage()
 
     def save(self, name, content):
-        non_gzipped_file_content = content.file
-        name = super(CachedS3BotoStorage, self).save(name, content)
-        content.file = non_gzipped_file_content
+        name = super().save(name, content)
         self.local_storage._save(name, content)
         return name
+
+    def url(self, name, headers=None, response_headers=None, expire=None):
+        base_url = super().url(name, headers=headers, response_headers=response_headers, expire=expire)
+
+        if name.endswith('.js') or name.endswith('.css'):
+            the_hash = self.get_hash(name)
+            sep = '%' if '?' in base_url else '?'
+            base_url = f'{base_url}{sep}{the_hash}'
+
+        return base_url
+
+    def get_hash(self, name):
+        git_hash = os.environ.get('HEROKU_SLUG_COMMIT', '')
+        return sha1(f'{git_hash}-{name}'.encode()).hexdigest()[:8]
 
 
 # We need a custom Storage Class for our Media files because we
