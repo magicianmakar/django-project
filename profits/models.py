731fa53c10f1b1812b1114d0861bdf5bcd62e6ba
Guilherme <guilherme.dcosta@gmail.com>
2019-08-14 13:10:34 -0300

    Increase facebook API version (#691)

--- profits/models.py
+++ profits/models.py
@@ -71,7 +71,7 @@ class FacebookAccess(models.Model):
             settings.FACEBOOK_APP_ID,
             settings.FACEBOOK_APP_SECRET,
             self.access_token,
-            api_version='v3.2'
+            api_version='v3.3'
         )
 
     @property
