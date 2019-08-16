9fc75d54210348bfce4d353f6a3322a6f3d42cd1
Umair Khan <umair.waheed@gmail.com>
2019-08-16 16:21:53 +0500

    home: Create separate urls.

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 import home.views
 
 urlpatterns = [
-    url(r'^(?:settings)?$', home.views.home_page_view, name='index'),
+    url(r'^$', home.views.home_page_view, name='index'),
+    url(r'^settings$', home.views.home_page_view, name='settings'),
 ]
