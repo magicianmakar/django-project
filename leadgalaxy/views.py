d90a3af79bee417b737fb267f996870b4ddddc10
Marian Gheata <victor960035@gmail.com>
2019-12-11 16:20:36 +0000

    Fix variable name collision in Alerts view

--- leadgalaxy/views.py
+++ leadgalaxy/views.py
@@ -4750,7 +4750,7 @@ def product_alerts(request):
     # Delete sidebar alert info cache
     cache.delete(make_template_fragment_key('alert_info', [request.user.id]))
 
-    tpl = 'product_alerts_tab.html' if product else 'product_alerts.html'
+    tpl = 'product_alerts_tab.html' if request.GET.get('product') else 'product_alerts.html'
     return render(request, tpl, {
         'product_changes': product_changes,
         'show_hidden': show_hidden,
--- woocommerce_core/views.py
+++ woocommerce_core/views.py
@@ -186,7 +186,7 @@ def product_alerts(request):
     # Delete sidebar alert info cache
     cache.delete(make_template_fragment_key('alert_info', [request.user.id]))
 
-    tpl = 'woocommerce/product_alerts_tab.html' if product else 'woocommerce/product_alerts.html'
+    tpl = 'woocommerce/product_alerts_tab.html' if request.GET.get('product') else 'woocommerce/product_alerts.html'
     return render(request, tpl, {
         'product_changes': product_changes,
         'show_hidden': show_hidden,
