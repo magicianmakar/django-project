cc1c3168126969c3237afc89d8f511075a57417d
Marian Gheata <victor960035@gmail.com>
2020-01-28 18:21:05 +0000

    Fix navigation to staff users

--- leadgalaxy/side_menu.py
+++ leadgalaxy/side_menu.py
@@ -156,7 +156,7 @@ def get_menu_item_data():
             'match': re.compile(r'(/\w+)?/pages/content/tools-business-tools'),
         },
         'academy': {
-            'title': '<span id="academy-span">Dropshipping 101</span><br>Step by Step Course',
+            'title': '<span id="academy-span">Dropshipping 101</span>',
             'url': 'https://academy.dropified.com/',
         },
         'video_training': {
