8d566ece3bd2add6d7af360ddc22542dea85e136
Marian Gheata <victor960035@gmail.com>
2020-02-07 15:24:47 +0000

    Update bot detection in Product Feeds

--- product_feed/utils.py
+++ product_feed/utils.py
@@ -2,13 +2,21 @@ from django.utils import timezone
 
 
 def is_bot_useragent(request):
-    ua = request.META.get('HTTP_USER_AGENT', '')
-    return any(['facebookexternalhit' in ua, 'googlebot' in ua])
+    ua = request.META.get('HTTP_USER_AGENT', '').lower()
+
+    if 'facebookexternalhit' in ua:
+        return 'fb'
+    elif 'googlebot' in ua or 'google-xrawler' in ua:
+        return 'google'
+    else:
+        return None
 
 
 def update_feed_social_access_at(feed, request):
-    if 'facebookexternalhit' in request.META.get('HTTP_USER_AGENT', '') or request.GET.get('f') == '1':
+    ua_name = is_bot_useragent(request)
+
+    if ua_name == 'fb' or request.GET.get('f') == '1':
         feed.fb_access_at = timezone.now()
 
-    if 'googlebot' in request.META.get('HTTP_USER_AGENT', '').lower():
+    if ua_name == 'google' or request.GET.get('f') == 'g':
         feed.google_access_at = timezone.now()
