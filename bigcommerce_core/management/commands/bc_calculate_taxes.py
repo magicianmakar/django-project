843814bce6b70072516e8782bd9e6d3bd841aaec
Marian Gheata <victor960035@gmail.com>
2020-05-16 21:02:29 +0000

    BigCommerce: Run revenue report on first day of month only

--- bigcommerce_core/management/commands/bc_calculate_taxes.py
+++ bigcommerce_core/management/commands/bc_calculate_taxes.py
@@ -1,16 +1,22 @@
-from django.contrib.auth.models import User
-from shopified_core.management import DropifiedBaseCommand
-from bigcommerce_core.models import BigCommerceStore
+import csv
+import os
+from io import StringIO
+from datetime import datetime
 
+import arrow
 from raven.contrib.django.raven_compat.models import client as raven_client
+
 from django.conf import settings
 from django.core.mail import EmailMessage
-from datetime import datetime
-import csv
-from io import StringIO
-import os
+from django.contrib.auth.models import User
 from django.template import Context, Template
+
+from shopified_core.management import DropifiedBaseCommand
+from bigcommerce_core.models import BigCommerceStore
 from shopified_core.utils import safe_float
+
+REVENUE_TAX_PERCENT = 20
+REVENUE_REPORT_EMAIL = 'bigcommerce@dropified.com'
 
 
 class Command(DropifiedBaseCommand):
@@ -20,7 +26,7 @@ class Command(DropifiedBaseCommand):
         parser.add_argument(
             '-email',
             '--email',
-            default=settings.BIGCOMMERCE_REVENUE_REPORT_EMAIL,
+            default=REVENUE_REPORT_EMAIL,
             help='email to send report to'
         )
         parser.add_argument(
@@ -31,10 +37,13 @@ class Command(DropifiedBaseCommand):
         )
 
     def start_command(self, *args, **options):
+        if arrow.utcnow().day != 1:
+            return
+
         if options['user_id']:
             users = User.objects.filter(pk=options['user_id'])
         else:
-            users = User.objects.filter(pk__in=BigCommerceStore.objects.values_list('user', flat=True))
+            users = User.objects.filter(pk__in=BigCommerceStore.objects.filter(is_active=True).values_list('user', flat=True))
 
         attachment_csv_file = StringIO()
         writer = csv.writer(attachment_csv_file)
@@ -53,14 +62,13 @@ class Command(DropifiedBaseCommand):
                 active_stores = user.bigcommercestore_set.filter(is_active=True)
                 # processing only users with BG stores
                 if active_stores.count() > 0:
-                    self.write(f"Processing User ID:{user.pk} ")
                     for bc_store in active_stores:
                         data_item = []
                         data_item.append(settings.BIGCOMMERCE_APP_ID)
                         data_item.append(bc_store.api_url)
                         data_item.append(bc_store.title)
                         data_item.append(user.email)
-                        data_item.append(user.first_name + " " + user.last_name)
+                        data_item.append(f'{user.first_name} {user.last_name}')
                         data_item.append(user.date_joined.strftime("%Y-%m-%d %H:%M:%S"))
                         monthly_revenue = safe_float(self.get_monthly_revenue(user))
                         if user.profile.on_trial:
@@ -68,7 +76,7 @@ class Command(DropifiedBaseCommand):
                             data_item.append('Yes')
                         else:
                             monthly_revenue_share = safe_float(monthly_revenue / active_stores.count()) * \
-                                (safe_float(settings.BIGCOMMERCE_REVENUE_TAX_PERCENT) / 100)
+                                (safe_float(REVENUE_TAX_PERCENT) / 100)
                             data_item.append('No')
                         data_item.append('${:.2f}'.format(monthly_revenue))
                         data_item.append('${:.2f}'.format(monthly_revenue_share))
