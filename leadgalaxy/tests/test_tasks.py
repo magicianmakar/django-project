45a4a9d488f1a9a1b08f681c2ff1694955378ea2
Nin King <bigninking@gmail.com>
2020-06-10 14:44:20 -0500

    Fix inventory syncing for shipping from Aliexpress (DROP-1963) #1072
    * Fix inventory syncing for shiping from ali
    
    * Add test for sync with supplier
    
    * Cleanups
    
    * Fix on product alerts utils import
    
    * Revert "Fix on product alerts utils import"
    
    This reverts commit beb4795571e8894d598281272f7375c206f2c764.
    
    * Get quantity directly from Aliexpress
    
    Co-authored-by: Ahmed Saidi <ma7dev@gmail.com>

--- leadgalaxy/tests/test_tasks.py
+++ leadgalaxy/tests/test_tasks.py
@@ -6,10 +6,13 @@ from leadgalaxy.tests.factories import (
     ShopifyProductFactory,
     ShopifyStoreFactory,
     UserFactory,
+    ProductSupplierFactory
 )
 
+from product_alerts.utils import get_supplier_variants
 from leadgalaxy.tasks import (
     link_variants_to_new_images,
+    sync_shopify_product_quantities
 )
 
 
@@ -66,3 +69,55 @@ class TasksTestCase(BaseTestCase):
         new_images = updated_new_data['product']['images']
         self.assertEqual(new_images[0]['src'], new_src)
         self.assertEqual(new_images[0]['variant_ids'], [1, 2])
+
+    def test_sync_inventory_with_supplier(self):
+        product = ShopifyProductFactory(
+            store=self.store, user=self.user, source_id=12345678,
+            data='''{"store": {
+                "name": "Suplier 1",
+                "url": "https://www.aliexpress.com/item/32815997098.html"
+            }}''')
+
+        supplier = ProductSupplierFactory(product=product, product_url='https://www.aliexpress.com/item/32815997098.html')
+
+        product.default_supplier = supplier
+        product.variants_map = json.dumps({
+            '32146941476915': json.dumps([
+                {
+                    'title': 'CHINA',
+                    'sku': '200007763:201336100'
+                },
+                {
+                    'title': '19 PCS Yellow',
+                    'sku': '14:365462'
+                }
+            ]),
+        })
+        product.save()
+
+        product_data = dict(
+            variants=[{
+                'id': 32146941476915,
+                'product_id': 4566069936179,
+                'title': '19 PCS Yellow',
+                'price': '42.95',
+                'sku': '14:365462',
+                'inventory_policy': 'deny',
+                'option1': '19 PCS Yellow',
+                'option2': None,
+                'option3': None,
+                'inventory_item_id': 34005719449651,
+                'inventory_quantity': 7167,
+                'old_inventory_quantity': 7167,
+            }]
+        )
+
+        aliexpress_data = get_supplier_variants('aliexpress', supplier.get_source_id())
+
+        with patch('leadgalaxy.models.ShopifyProduct.set_variant_quantity') as mock_set_variant_quantity, \
+                patch('leadgalaxy.utils.get_shopify_product', return_value=product_data):
+            sync_shopify_product_quantities(product.id)
+            mock_set_variant_quantity.assert_called_with(
+                quantity=aliexpress_data[1]['availabe_qty'],
+                variant=product_data['variants'][0]
+            )
