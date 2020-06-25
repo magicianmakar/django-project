cee7ae734415ac769f029cefe2ce3355fd9a4117
Guilherme <guilherme.dcosta@gmail.com>
2020-06-25 17:16:24 -0300

    Clean up code

--- supplements/lib/authorizenet.py
+++ supplements/lib/authorizenet.py
@@ -147,29 +147,20 @@ def charge_customer_profile(amount, customer_id, payment_id, lines):
     response = controller.getresponse()
 
     # https://developer.authorize.net/api/reference/index.html#payment-transactions-charge-a-credit-card
-    if response.messages.resultCode == "Ok":
-        # Since the API request was successful, look for a transaction response
-        # and parse it to display the results of authorizing the card
-        if hasattr(response.transactionResponse, 'messages'):
-            return response.transactionResponse.transId
-        elif hasattr(response.transactionResponse, 'errors'):
-            errors = []
-            for error in response.transactionResponse.errors.error:
-                errors.append(f"{error.errorCode}: {error.errorText}")
-            capture_message('Auth.NET transaction error', extra={'authnet-errors': errors}, level='fatal')
+    errors = []
+    if response.messages.resultCode == "Ok" \
+            and hasattr(response.transactionResponse, 'messages'):
+        return response.transactionResponse.transId
 
-    else:
-        if hasattr(response, 'transactionResponse') \
-                and hasattr(response.transactionResponse, 'errors'):
-            errors = []
-            for error in response.transactionResponse.errors.error:
-                errors.append(f"{error.errorCode}: {error.errorText}")
-            capture_message('Auth.NET transaction error', extra={'authnet-errors': errors}, level='fatal')
-        else:
-            errors = []
-            for error in response.messages.message:
-                errors.append(f"{error['code'].text}: {error['text'].text}")
-            capture_message('Auth.NET transaction error', extra={'authnet-errors': errors}, level='fatal')
+    if not hasattr(response, 'transactionResponse'):
+        for error in response.messages.message:
+            errors.append(f"{error['code'].text}: {error['text'].text}")
+    elif hasattr(response.transactionResponse, 'errors'):
+        for error in response.transactionResponse.errors.error:
+            errors.append(f"{error.errorCode}: {error.errorText}")
+
+    if errors:
+        capture_message('Auth.NET transaction error', extra={'authnet-errors': errors})
 
 
 def get_customer_payment_profile(profile_id, payment_id):
