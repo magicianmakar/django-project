075e590884a6c8ca22d40933037425175aa42126
Marian Gheata <victor960035@gmail.com>
2020-07-06 22:01:08 +0000

    Update used API version

--- leadgalaxy/models.py
+++ leadgalaxy/models.py
@@ -28,7 +28,7 @@ from product_alerts.utils import monitor_product
 from shopified_core.decorators import add_to_class, upsell_page_permissions
 from shopified_core.models import StoreBase, ProductBase, SupplierBase, BoardBase, OrderTrackBase, UserUploadBase
 
-SHOPIFY_API_VERSION = "2019-07"
+SHOPIFY_API_VERSION = "2020-01"
 
 ENTITY_STATUS_CHOICES = (
     (0, 'Pending'),
