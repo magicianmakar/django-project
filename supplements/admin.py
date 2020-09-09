bca526421b384b70330176a91082f7c0958a566f
Guilherme <guilherme.dcosta@gmail.com>
2020-09-09 20:53:22 -0300

    Add supplement orders detailed report in admin (#1256)

--- supplements/admin.py
+++ supplements/admin.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import csv
 from django.contrib import admin
+from django.contrib.admin import helpers
+from django.http import HttpResponse
 
 from .forms import ShippingGroupAdminForm
 from .models import (
@@ -90,8 +93,27 @@ class LabelCommentAdmin(admin.ModelAdmin):
     raw_id_fields = ('label', 'user')
 
 
+class OrderPaidFilter(admin.SimpleListFilter):
+    title = 'Payment Status'
+    parameter_name = 'payment_status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('paid', 'Paid'),
+            ('unpaid', 'Not Paid'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'unpaid':
+            return queryset.filter(stripe_transaction_id='')
+        if self.value() == 'paid':
+            return queryset.exclude(stripe_transaction_id='')
+
+
 @admin.register(PLSOrder)
 class PLSOrderAdmin(admin.ModelAdmin):
+    list_filter = (OrderPaidFilter,)
+    search_fields = ('user__id', 'user__email')
     list_display = (
         'id',
         'order_number',
@@ -110,9 +132,58 @@ class PLSOrderAdmin(admin.ModelAdmin):
     )
     raw_id_fields = ('user',)
     readonly_fields = ('stripe_transaction_id', 'payment_date')
+    actions = ('export_orders',)
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def changelist_view(self, request, extra_context=None):
+        # Hack for accepting empty _selected_action
+        if request.POST.get('action') == 'export_orders':
+            if not request.POST.getlist(helpers.ACTION_CHECKBOX_NAME):
+                post = request.POST.copy()
+                post.setlist(helpers.ACTION_CHECKBOX_NAME, [0])
+                request.POST = post
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def export_orders(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="detailed-orders.csv"'
+
+        # We need a clean queryset in order to use all objects
+        ids = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+        if len(ids) == 1 and ids[0] == 0:
+            cl = self.get_changelist_instance(request)
+            queryset = cl.get_queryset(request)
+
+        writer = csv.writer(response)
+        # order id, quantity x supplement, total cost
+        writer.writerow([
+            'User ID',
+            'Order ID',  # 1
+            'Line ID',  # 2
+            'Supplement',  # 3
+            'Total Cost',  # 4
+        ])
+
+        orders = queryset.prefetch_related(
+            'order_items',
+            'order_items__label',
+            'order_items__label__user_supplement',
+        )
+        for order in orders:
+            for item in order.order_items.all():
+                writer.writerow([
+                    order.user_id,
+                    order.order_number,
+                    item.line_id,
+                    f'{item.quantity} x {item.label.user_supplement.title} (${item.amount / 100.:.2f})',
+                    f'${((item.amount * item.quantity) / 100.):.2f}'
+                ])
+
+        return response
+    export_orders.short_description = "Export order details"
 
 
 @admin.register(PLSOrderLine)
