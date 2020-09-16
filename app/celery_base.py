029c81883d57584594f14ab78600e5a7f1211e6d
Marian Gheata <victor960035@gmail.com>
2020-09-16 01:10:01 +0000

    Upgrade Celery

--- app/celery_base.py
+++ app/celery_base.py
@@ -29,7 +29,7 @@ celery_app = CeleryBase('shopified')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-celery_app.config_from_object('django.conf:settings')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # hook into the Celery error handler
