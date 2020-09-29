d6b4e7a27a523df70f332d1da9726a9d30a8d2d6
Marian Gheata <victor960035@gmail.com>
2020-09-29 19:24:34 +0000

    Fix Goals admin loading time

--- goals/admin.py
+++ goals/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Goal, Step, StepExtraAction
+from .models import Goal, UserGoalRelationship, GoalStepRelationship, Step, StepExtraAction
 
 
 class GoalStepRelationshipInline(admin.TabularInline):
@@ -13,17 +13,41 @@ class UserGoalRelationshipInline(admin.StackedInline):
     extra = 1
 
 
+@admin.register(UserGoalRelationship)
+class UserGoalRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'goal', 'viewed')
+    list_filter = ('viewed',)
+    raw_id_fields = ('user', 'goal')
+
+
+@admin.register(GoalStepRelationship)
+class GoalStepRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'goal', 'step', 'step_number')
+    raw_id_fields = ('goal', 'step')
+
+
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
-    list_display = 'title', 'description'
+    list_display = ('id', 'goal_number', 'title', 'description', 'tip')
     inlines = GoalStepRelationshipInline, UserGoalRelationshipInline
 
 
 @admin.register(Step)
 class StepAdmin(admin.ModelAdmin):
-    list_display = 'slug',
+    list_display = (
+        'id',
+        'title',
+        'description',
+        'slug',
+        'show_me_how_url',
+        'action_url',
+        'action_title',
+        'icon_src',
+    )
+    search_fields = ('slug',)
 
 
 @admin.register(StepExtraAction)
 class StepExtraActionAdmin(admin.ModelAdmin):
-    list_display = 'action_title',
+    list_display = ('id', 'step', 'action_url', 'action_title', 'icon_src')
+    raw_id_fields = ('step',)
