c80541787cfe089112b4e8c4a131ede8ba4bdb88
Marian Gheata <victor960035@gmail.com>
2020-10-08 23:54:37 +0000

    Fix migration fix

--- addons_core/migrations/0014_auto_20200923_2206.py
+++ addons_core/migrations/0014_auto_20200923_2206.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('addons_core', '0012_auto_20200826_2337'),
+        ('addons_core', '0013_auto_20200930_1638'),
     ]
 
     operations = [
