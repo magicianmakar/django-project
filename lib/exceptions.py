4f737905d900251d96f75f6e594611da9df1f07a
Marian Gheata <victor960035@gmail.com>
2020-11-10 22:21:15 +0000

    Format printing exceptions

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -8,14 +8,36 @@ from raven.contrib.django.raven_compat.models import client
 
 def capture_exception(*args, **options):
     if settings.DEBUG or 'test' in sys.argv:
-        print('[Capture Exception]:', *args, options)
-        traceback.print_exc()
+        print()
+        print('------> Capture Exception')
+        print('------>')
+
+        if args:
+            print('------> args:', *args)
+
+        if options:
+            print('------> options:', options)
+
+        print('------>')
+        for line in traceback.format_exc().split('\n'):
+            print(f'------> {line}')
+        print('------>')
 
     client.captureException(*args, **options)
 
 
 def capture_message(*args, **options):
     if settings.DEBUG or 'test' in sys.argv:
-        print('[Capture Message]:', *args, options)
+        print()
+        print('------> Capture Message')
+        print('------>')
+
+        if args:
+            print('------> args:', *args)
+
+        if options:
+            print('------> options:', options)
+
+        print('------>')
 
     client.captureMessage(*args, **options)
