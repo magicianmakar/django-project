632eba3b9be47e51bcbbaa182eb6ea1fd960058b
Guilherme <guilherme.dcosta@gmail.com>
2020-11-25 16:48:47 -0300

    Add intercom class to open chat and fix upsell cookies (#1344)

--- addons_core/static/addons/js/addons-upsells.js
+++ addons_core/static/addons/js/addons-upsells.js
@@ -1,16 +1,15 @@
 var selected_addons_upsells = Cookies.get('drop-addons-upsells');
-selected_addons_upsells = JSON.parse(decodeURIComponent(selected_addons_upsells));
+if (selected_addons_upsells) {
+    selected_addons_upsells = JSON.parse(decodeURIComponent(selected_addons_upsells));
 
-for (var addon in selected_addons_upsells) {
-    console.log(selected_addons_upsells[addon]);
-
-    $.ajax(api_url('install', 'addons'),
-                {
-                    type: 'post',
-                    data: {
-                        addon: selected_addons_upsells[addon]['id']
-                    }
-                });
+    for (var addon in selected_addons_upsells) {
+        $.ajax({
+            url: api_url('install', 'addons'),
+            type: 'post',
+            data: {
+                addon: selected_addons_upsells[addon]['id']
+            }
+        });
+    }
+    Cookies.remove('drop-addons-upsells');
 }
-
-Cookies.remove('drop-addons-upsells');
