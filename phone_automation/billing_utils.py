71831e06a91da09470923375db0b46ab5c5c304e
Ievgeniy Tsymbaliuk (jakerator) <jakerator1@gmail.com>
2020-11-27 19:40:37 +0200

    exclude "released" numbers from overages (fix) (#1367)

--- phone_automation/billing_utils.py
+++ phone_automation/billing_utils.py
@@ -81,17 +81,19 @@ class CallflexOveragesBilling:
         phonenumber_usage_local = utils.get_phonenumber_usage(self.user, "local")
 
         if phonenumber_usage_tollfree['total'] is not False and phonenumber_usage_tollfree['used'] >= phonenumber_usage_tollfree['total']:
-            overage_phones = self.user.twilio_phone_numbers.filter(type="tollfree").all()[safe_int(phonenumber_usage_tollfree['total']):]
-            # going thru each "overage" number and calculating monther passed (after provisioining or subscription start, which was first
+            overage_phones = self.user.twilio_phone_numbers.filter(type="tollfree").exclude(status='released'). \
+                all()[safe_int(phonenumber_usage_tollfree['total']):]
+            # going thru each "overage" number and calculating monthes passed (after provisioining or subscription start, which was first )
             for overage_phone in overage_phones:
                 if overage_phone.created_at > subscription_period_start:
                     month_passed_phone = utils.get_monthes_passed(overage_phone.created_at)
                 else:
                     month_passed_phone = month_passed
-                    overages_phone_number += settings.EXTRA_TOLLFREE_NUMBER_PRICE * (month_passed_phone + 1)
+                overages_phone_number += settings.EXTRA_TOLLFREE_NUMBER_PRICE * (month_passed_phone + 1)
 
         if phonenumber_usage_local['total'] is not False and phonenumber_usage_local['used'] >= phonenumber_usage_local['total']:
-            overage_phones = self.user.twilio_phone_numbers.filter(type="local").all()[safe_int(phonenumber_usage_tollfree['total']):]
+            overage_phones = self.user.twilio_phone_numbers.filter(type="local").exclude(status='released'). \
+                all()[safe_int(phonenumber_usage_tollfree['total']):]
             # now the same for local numbers
             for overage_phone in overage_phones:
                 if overage_phone.created_at > subscription_period_start:
