1e8a1981fa0f5caf08f0f16ae21eec9139279471
Ievgeniy Tsymbaliuk (jakerator) <jakerator1@gmail.com>
2020-12-02 17:43:12 +0200

    Added permissions check on SSO (for LearnDash) (#1350)
    Co-authored-by: Ahmed Saidi <ma7dev@gmail.com>

--- sso_core/views.py
+++ sso_core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, JsonResponse
 from shopified_core.utils import get_domain
+import simplejson as json
 
 
 def _generate_jwt_token(user):
@@ -27,7 +28,7 @@ def redirect(request):
     token = _generate_jwt_token(request.user)
     redirect = request.GET.get('redirect', 'http://academy.dropified.com/sso')
 
-    if get_domain(redirect) != 'dropified' and get_domain(redirect, full=True) != 'challengedev.wpengine.com':
+    if not settings.DEBUG and get_domain(redirect) != 'dropified' and get_domain(redirect, full=True) != 'challengedev.wpengine.com':
         return JsonResponse({'error': 'Domain is not allowed'}, status=403)
 
     return HttpResponseRedirect('{}?token={}'.format(redirect, token))
@@ -49,11 +50,22 @@ def validate(request):
         return JsonResponse({'error': 'Token have expired'}, status=406)
 
     user = User.objects.get(id=info['id'])
+
+    permissions = request.POST.get('permissions')
+    response_permissions = {}
+    try:
+        permissions = json.loads(permissions)
+        for permission in permissions:
+            response_permissions[permission] = user.can(permission)
+    except:
+        pass
+
     info.update({
         'email': user.email,
         'first_name': user.first_name,
         'last_name': user.last_name,
         'is_black': user.can('pls.use') or user.is_staff,
+        'response_permissions': response_permissions,
     })
 
     return JsonResponse(info)
