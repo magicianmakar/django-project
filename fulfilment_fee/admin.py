2c1573580dfa424c281c43b91d2117d9c54c8759
Ievgeniy Tsymbaliuk (jakerator) <jakerator1@gmail.com>
2020-12-22 19:09:46 +0200

    Updates to fulfilment fees admin (DROP-2379) (#1397)
    * updates to fulfilment fees admin
    
    * Fix file name.
    
    Co-authored-by: Umair Khan <umair@thedevelopmentmachine.com>

--- fulfilment_fee/admin.py
+++ fulfilment_fee/admin.py
@@ -1,8 +1,33 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils import timezone
+
 from .models import (
     SalesFeeConfig,
-    SaleTransactionFee
+    SaleTransactionFee,
 )
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        file_name = f'Sale Transaction Fee - {timezone.now()}.csv'
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={file_name}'
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 @admin.register(SalesFeeConfig)
@@ -13,5 +38,8 @@ class SalesFeeConfigAdmin(admin.ModelAdmin):
 
 @admin.register(SaleTransactionFee)
 class SaleTransactionFeeAdmin(admin.ModelAdmin):
-    list_display = ('source_model', 'source_id', 'fee_value')
-    list_filter = ('source_model', 'source_id')
+    list_display = ('source_model', 'source_id', 'fee_value', 'user', 'created_at')
+    list_filter = ('source_model', 'source_id', 'user', 'created_at')
+    raw_id_fields = ('user',)
+    search_fields = ('user__email', 'source_id', 'created_at')
+    actions = (ExportCsvMixin.export_as_csv,)
