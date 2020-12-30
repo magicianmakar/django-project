9df52eb17e9c198169e1bee8aac610f322ae4eb8
Ievgeniy Tsymbaliuk (jakerator) <jakerator1@gmail.com>
2020-12-30 16:37:26 +0200

    Sales fees log page update (DROP-2385) #1406

--- fulfilment_fee/views.py
+++ fulfilment_fee/views.py
@@ -9,8 +9,10 @@ from datetime import timedelta
 def fees_list(request):
 
     if request.user.can('sales_fee.use') and (request.user.is_superuser or not request.user.can('disabled_sales_fee.use')):
-        exp_date = timezone.now() + timedelta(days=-30)
+        exp_date = timezone.now() + timedelta(days=-90)
 
         fees = request.user.saletransactionfee_set.filter(created_at__gte=exp_date)
+    else:
+        fees = []
 
     return render(request, 'fulfilment_fee/fees_list.html', {'fees': fees})
