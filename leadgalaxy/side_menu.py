15688e171a0afea54afd40e41ae07f7324af593a
Guilherme da Costa <guilherme.dcosta@gmail.com>
2021-03-18 19:37:14 -0300

    Fix alibaba side menu url (#1504)

--- leadgalaxy/side_menu.py
+++ leadgalaxy/side_menu.py
@@ -104,6 +104,7 @@ def get_menu_item_data(request):
             'url_name': 'alibaba:products',
             'permissions': ['alibaba_integration.use'],
             'match': re.compile(r'(/\w+)?/alibaba/products'),
+            'is_ns_aware': False,
             'new_tab': True,
         },
         'boards': {
