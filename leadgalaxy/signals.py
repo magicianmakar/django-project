5fe83f8d6bbce5441561978050d0e2cbdcf65c30
Marian Gheata <victor960035@gmail.com>
2021-03-29 22:27:30 +0000

    Handle an exception

--- leadgalaxy/signals.py
+++ leadgalaxy/signals.py
@@ -60,8 +60,11 @@ def update_plan_changed_date(sender, instance, created, **kwargs):
     except:
         return
 
-    if current_plan and current_plan.is_shopify() and change_log.plan.is_shopify() and current_plan != change_log.plan:
-        post_churnzero_change_plan_event(instance.user, current_plan.title)
+    try:
+        if current_plan and current_plan.is_shopify() and change_log.plan.is_shopify() and current_plan != change_log.plan:
+            post_churnzero_change_plan_event(instance.user, current_plan.title)
+    except:
+        pass
 
     if current_plan != change_log.plan:
         change_log.previous_plan = change_log.plan
