3399732079b642156a612ca0b087cd60263f9089
Marian Gheata <victor960035@gmail.com>
2021-03-31 19:41:08 +0000

    USe progress for update command

--- leadgalaxy/management/commands/update_churnzero_account.py
+++ leadgalaxy/management/commands/update_churnzero_account.py
@@ -2,16 +2,19 @@ from shopified_core.management import DropifiedBaseCommand
 from django.contrib.auth.models import User
 from churnzero_core.utils import set_churnzero_account
 
+#  55472/119912
+
 
 class Command(DropifiedBaseCommand):
     def start_command(self, *args, **options):
-        for user in User.objects.filter(profile__subuser_parent__isnull=True):
-            if user.profile.plan:
-                if user.profile.plan.is_stripe() or user.profile.plan.is_shopify():
-                    self.write(f"Updating ChurnZero account for {user.username}")
-                    try:
-                        set_churnzero_account(user)
-                    except Exception:
-                        self.write("Update failed")
-                    else:
-                        self.write("Update successful")
+        users = User.objects.filter(profile__subuser_parent__isnull=True)
+        self.progress_total(users.count())
+        for user in users:
+            self.progress_update()
+
+            try:
+                if user.profile.plan and (user.profile.plan.is_stripe() or user.profile.plan.is_shopify()):
+                    set_churnzero_account(user)
+
+            except Exception:
+                self.write(f'Error for {user.email}')
