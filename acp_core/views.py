ed348ef6053e2615c6ee0a970eba984081abe783
Marian Gheata <victor960035@gmail.com>
2021-04-24 00:05:58 +0000

    Update Stripe version

--- acp_core/views.py
+++ acp_core/views.py
@@ -204,7 +204,7 @@ class ACPUserSearchView(BaseTemplateView):
 
             invoices = {}
             if customer_id:
-                for i in stripe.Charge.list(limit=10, customer=customer_id).data:
+                for i in stripe.Charge.list(limit=10, customer=customer_id, expand=['dispute']).data:
                     charge = {
                         'id': i.id,
                         'date': arrow.get(i.created).format('MM/DD/YYYY HH:mm'),
--- stripe_subscription/stripe_api.py
+++ stripe_subscription/stripe_api.py
@@ -3,4 +3,4 @@ from django.conf import settings
 import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
-stripe.api_version = '2016-06-15'
+stripe.api_version = '2018-01-23'
