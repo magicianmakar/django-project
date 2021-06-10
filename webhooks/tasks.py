42d26a6435e22ea1f17b9f0c6c3cefa74391a6ed
Marian Gheata <victor960035@gmail.com>
2021-06-10 18:52:45 +0000

    Fix phone setup

--- webhooks/tasks.py
+++ webhooks/tasks.py
@@ -2,13 +2,19 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 from app.celery_base import celery_app, CaptureFailure
+from leadgalaxy.models import GroupPlan
 from metrics.activecampaign import ActiveCampaignAPI
 
 
 @celery_app.task(base=CaptureFailure, ignore_result=True)
 def setup_free_account(user_id):
     user = User.objects.get(id=user_id)
-    phone = user.get_config('_phone')
+    phone = user.get_config('__phone')
+
+    if user.get_config('__plan'):
+        free_plan = GroupPlan.objects.get(id=user.get_config('__plan'))
+        if user.profile.plan != free_plan and user.profile.plan.free_plan:
+            user.profile.change_plan(free_plan)
 
     if settings.ACTIVECAMPAIGN_KEY and phone:
         api = ActiveCampaignAPI()
