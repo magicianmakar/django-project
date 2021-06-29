5a1a06c096919543def483ab0a89253a704f9864
Amardeep-Singh <Amardeep-Singh@users.noreply.github.com>
2021-06-30 04:03:47 +0530

    View Sidebar links for Feeds and Sub Users with .view permissions  (DROP-2713) (#1627)
    * Add feeds permission to upsell exception list
    
    * Flake8 fix

--- leadgalaxy/side_menu.py
+++ leadgalaxy/side_menu.py
@@ -246,7 +246,8 @@ def create_menu(menu_structure, menu_data, request, namespace):
 
     # Know which bypass instead of removing these permissions from menu
     upsell_permission_exceptions = ['price_changes.use', 'profit_dashboard.view',
-                                    'mapping_bundle.use', 'suppliers_shipping_mapping.use', 'sub_users.use']
+                                    'mapping_bundle.use', 'suppliers_shipping_mapping.use', 'sub_users.use',
+                                    'product_feeds.use', 'google_product_feed.use']
     menu = []
     for section_key, item_keys in menu_structure:
         section = menu_data[section_key]
