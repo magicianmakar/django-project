f41152bcf6eddb083225e1578a6b308ddc12369a
Umair Khan <umair@thedevelopmentmachine.com>
2021-10-08 20:31:56 +0500

    Minimize number of invoice items for Sales Fees (DROP-2904) (#1737)
    * Minimize number of invoice items for Sales Fees.
    
    Fixes DROP-2904
    
    * Fix empty charge.
    
    * Remove debugger.

--- fulfilment_fee/management/commands/process_sales_fees.py
+++ fulfilment_fee/management/commands/process_sales_fees.py
@@ -27,7 +27,9 @@ class Command(DropifiedBaseCommand):
         sale_transaction_fees = sale_transaction_fees.all()
 
         skip_user_ids = []
+        sales_fees = []
         for sale_transaction_fee in sale_transaction_fees:
+            charge = {}
             try:
                 if sale_transaction_fee.user.is_stripe_customer() and sale_transaction_fee.user.id not in skip_user_ids:
                     try:
@@ -61,23 +63,56 @@ class Command(DropifiedBaseCommand):
 
                             custom_stripe_subscription.save()
 
-                        upcoming_invoice_item = add_invoice(sub_container, 'sale_fee',
-                                                            sale_transaction_fee.fee_value, False, "Order Sales Fee")
-                        if upcoming_invoice_item:
-                            sale_transaction_fee.processed = True
-                            sale_transaction_fee.save()
+                        if sales_fees:
+                            for fee in sales_fees:
+                                if fee['user'] == sale_transaction_fee.user:
+                                    fee['fee'] += sale_transaction_fee.fee_value
+                                    fee['trans_obj'].append(sale_transaction_fee)
+                        else:
+                            charge['source'] = 'stripe'
+                            charge['user'] = sub_container
+                            charge['fee'] = sale_transaction_fee.fee_value
+                            charge['trans_obj'] = [sale_transaction_fee]
                     except:
                         skip_user_ids.append(sale_transaction_fee.user.id)
                         capture_exception()
 
                 elif sale_transaction_fee.user.profile.from_shopify_app_store() and sale_transaction_fee.user.id not in skip_user_ids:
-                    # shopify billing (using usage charge api)
-                    charge_id = add_shopify_usage_invoice(sale_transaction_fee.user, 'sale_fee', sale_transaction_fee.fee_value,
-                                                          "Order Sales Fee")
-                    if charge_id:
-                        sale_transaction_fee.processed = True
-                        sale_transaction_fee.save()
-                    else:
+                    try:
+                        if sales_fees:
+                            for fee in sales_fees:
+                                if fee['user'] == sale_transaction_fee.user:
+                                    fee['fee'] += sale_transaction_fee.fee_value
+                                    fee['trans_obj'].append(sale_transaction_fee)
+                        else:
+                            charge['source'] = 'shopify'
+                            charge['user'] = sale_transaction_fee.user
+                            charge['fee'] = sale_transaction_fee.fee_value
+                            charge['trans_obj'] = [sale_transaction_fee]
+                    except:
                         skip_user_ids.append(sale_transaction_fee.user.id)
+                if charge:
+                    sales_fees.append(charge)
+            except:
+                capture_exception()
+
+        for charge in sales_fees:
+            try:
+                if charge['source'] == 'stripe':
+                    upcoming_invoice_item = add_invoice(charge['user'], 'sale_fee',
+                                                        charge['fee'], False, 'Order Sales Fee')
+
+                    if upcoming_invoice_item:
+                        for obj in charge['trans_obj']:
+                            obj.processed = True
+                            obj.save()
+                elif charge['source'] == 'shopify':
+                    # shopify billing (using usage charge api)
+                    charge_id = add_shopify_usage_invoice(charge['user'], 'sale_fee',
+                                                          charge['fee'], 'Order Sales Fee')
+                    if charge_id:
+                        for obj in charge['trans_obj']:
+                            obj.processed = True
+                            obj.save()
             except:
                 capture_exception()
