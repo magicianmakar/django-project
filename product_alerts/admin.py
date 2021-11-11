3e0e0e88663ee6e2498e259cadd50c7b1b5aedc5
Umair Khan <umair@thedevelopmentmachine.com>
2021-11-11 16:28:55 +0500

    Add rawid fileds for store products. (#1787)

--- product_alerts/admin.py
+++ product_alerts/admin.py
@@ -9,5 +9,5 @@ USER_SEARCH_FIELDS = ('user__id', 'user__username', 'user__email')
 @admin.register(ProductChange)
 class ProductChangeAdmin(admin.ModelAdmin):
     list_display = ('user', 'store_type', 'status', 'created_at', 'updated_at', 'notified_at')
-    raw_id_fields = ('user', 'shopify_product', 'chq_product')
+    raw_id_fields = ('user', 'shopify_product', 'chq_product', 'gkart_product', 'woo_product', 'bigcommerce_product')
     search_fields = USER_SEARCH_FIELDS
