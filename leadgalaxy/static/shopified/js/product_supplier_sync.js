ce174c76dfd48efff1802461ce1f094d71a7eb27
Roman Koshovyi <93263822+romkosh@users.noreply.github.com>
2021-12-10 19:04:27 +0200

    Synchronization stop immediately (DROP-2822) (#1809)
    * last changes
    
    * last chanches to prettify code
    
    * Delete package-lock.json
    
    * deploy local server
    
    * Add Django Debug Toolbar
    
    * Add pgAdmin
    
    * Sync stop immediately after clicking the stop button (DROPM-118)
    
    * Revert "deploy local server"
    
    This reverts commit 6a6a0aaef24a1439ce48e6f3667d3ba896959bb9.
    
    * Revert "Add Django Debug Toolbar"
    
    This reverts commit 33414addcae4e0b7c392428121712bb89b62e856.
    
    * Revert "Add pgAdmin"
    
    This reverts commit 5d5589221720c7ec2200062f92a11e68dfb42129.
    
    Co-authored-by: Hanna20122 <sannaoua@gmail.com>
    Co-authored-by: Hanna20122 <37797452+Hanna20122@users.noreply.github.com>
    Co-authored-by: Nazik Almazova <nazikalm@gmail.com>

--- leadgalaxy/static/shopified/js/product_supplier_sync.js
+++ leadgalaxy/static/shopified/js/product_supplier_sync.js
@@ -120,6 +120,12 @@
 
     $('#modal-product-supplier-sync .stop-update-btn').click(function(e) {
         e.preventDefault();
+        
+        $('#modal-product-supplier-sync input').prop('disabled', null);
+        $('#modal-product-supplier-sync .start-update-btn').show();
+        $('#modal-product-supplier-sync .stop-update-btn').hide();
+        $('#modal-product-supplier-sync .update-progress').hide();
+        
         $.ajax({
             url: '/api/products-supplier-sync-stop',
             type: 'POST',
@@ -127,9 +133,8 @@
                 store: $(e.target).attr('data-store')
             },
         }).done(function(data) {
-            $('#modal-product-supplier-sync input').prop('disabled', null);
-            $('#modal-product-supplier-sync .start-update-btn').show();
-            $('#modal-product-supplier-sync .stop-update-btn').hide();
+            swal('Product Sync', 'Product Sync is stopped', 'success');
+            return;
 
         }).fail(function(data) {
             displayAjaxError('Product Sync', data);
