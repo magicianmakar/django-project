906f73e00e22eb20c83d3e9e67d85cd72910191e
Marian Gheata <victor960035@gmail.com>
2021-12-21 17:36:16 +0000

    Check if Tapfiliate API Key is set

--- tapfiliate/tasks.py
+++ tapfiliate/tasks.py
@@ -2,22 +2,25 @@
 
 from app.celery_base import celery_app, CaptureFailure
 
-from lib.exceptions import capture_exception
+from django.conf import settings
 
+from lib.exceptions import capture_exception
 from tapfiliate.utils import add_commission_from_stripe, add_successful_payment
 
 
 @celery_app.task(base=CaptureFailure, bind=True, ignore_result=True)
 def commission_from_stripe(self, charge_id):
-    try:
-        add_commission_from_stripe(charge_id)
-    except:
-        capture_exception()
+    if settings.TAPFILIATE_API_KEY:
+        try:
+            add_commission_from_stripe(charge_id)
+        except:
+            capture_exception()
 
 
 @celery_app.task(base=CaptureFailure, bind=True, ignore_result=True)
 def successful_payment(self, charge_id):
-    try:
-        add_successful_payment(charge_id)
-    except:
-        capture_exception()
+    if settings.TAPFILIATE_API_KEY:
+        try:
+            add_successful_payment(charge_id)
+        except:
+            capture_exception()
