7348c895f4c00d4cb4d446b9fd533d7adb5a52ce
Guilherme da Costa <guilherme.dcosta@gmail.com>
2022-02-28 12:17:35 -0300

    Handle multi accounts for alibaba and add papertrail (DROP-3793) (#1955)

--- alibaba_core/management/commands/consume_alibaba_messages.py
+++ alibaba_core/management/commands/consume_alibaba_messages.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 from django.test import RequestFactory
 
-from alibaba_core.models import AlibabaOrder
+from alibaba_core.models import AlibabaAccount, AlibabaOrder
 from alibaba_core.utils import APIRequest, save_alibaba_products
 from lib.exceptions import capture_exception, capture_message
 from shopified_core.commands import DropifiedBaseCommand
@@ -35,17 +35,32 @@ class Command(DropifiedBaseCommand):
 
         messages = response['tmc_messages_consume_response']['messages']
         if messages:
+            self.write('[Alibaba] Messages received')
+            for m in messages['tmc_message']:
+                self.write(m)
             self.confirm_messages([m['id'] for m in messages['tmc_message']])
+            self.write('[Alibaba] Messages confirmed')
+
             for message in messages['tmc_message']:
+                self.write(f"[Alibaba] Message handled {message['id']}")
+
                 if message['topic'] == 'icbu_trade_ProductNotify':
                     alibaba_user_id = message['user_id']
                     content = json.loads(message['content'])
-                    products_data[alibaba_user_id].append(content['product_id'])
+                    accounts = AlibabaAccount.objects.filter(alibaba_user_id=alibaba_user_id)
+                    if not accounts:
+                        capture_message(
+                            'Alibaba Account does not exist.',
+                            extra={'alibaba_user_id': alibaba_user_id}
+                        )
+                        continue
+                    for account in accounts:
+                        products_data[account.user.id].append(content['product_id'])
 
                 if message['topic'] == 'icbu_trade_OrderNotify':
                     content = json.loads(message['content'])
-                    order = AlibabaOrder.objects.prefetch_related('items').filter(trade_id=content['trade_id']).first()
-                    if order:
+                    orders = AlibabaOrder.objects.prefetch_related('items').filter(trade_id=content['trade_id'])
+                    for order in orders:
                         order.reload_details()
                         order.handle_tracking()
 
