cbd9392d8298e91a8a7cf0c7e630a46cb1187bc1
Marian Gheata <victor960035@gmail.com>
2022-03-01 21:30:54 +0000

    Accurate revenue and Orders count

--- hubspot_core/management/commands/hubspot.py
+++ hubspot_core/management/commands/hubspot.py
@@ -10,14 +10,15 @@ import requests
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 from hubspot_core.models import HubspotAccount
-from hubspot_core.utils import INTERVALS, api_requests, clean_plan_name, create_contact, update_contact
+from hubspot_core.utils import api_requests, clean_plan_name, create_contact, update_contact
 from last_seen.models import LastSeen
-from leadgalaxy.models import GroupPlan, ShopifyStore, UserProfile
+from leadgalaxy.models import GroupPlan, UserProfile
 from lib.exceptions import capture_exception
 from shopified_core.commands import DropifiedBaseCommand
-from shopified_core.utils import safe_int, safe_float, safe_str
+from shopify_orders.models import ShopifyOrderRevenue
 
 
 def create_contact_worker(cmd, q):
@@ -44,6 +45,7 @@ class Command(DropifiedBaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--create', action='store_true', help='Create Properties only')
         parser.add_argument('--orders', action='store_true', help='Load orders stats')
+        parser.add_argument('--tracks', action='store_true', help='Load order tracking info')
         parser.add_argument('--mrr', action='store_true', help='Get MRR/LTV data')
         parser.add_argument('--missing', action='store_true', help='Add Missing users only')
         parser.add_argument('--skip', action='store', type=int, default=0, help='Skip this number of users')
@@ -55,17 +57,21 @@ class Command(DropifiedBaseCommand):
             self.write('Create Properties...')
             return self.create_properties()
 
-        if options['orders']:
+        elif options['orders']:
             self.write('Orders data...')
             return self.find_orders()
 
-        if options['mrr']:
+        elif options['mrr']:
             self.write('Get MRR data...')
             return self.find_mrr()
 
-        if options['admitad']:
+        elif options['admitad']:
             self.write('Load admitad data')
             return self.load_admitad(options['admitad'])
+
+        elif options['tracks']:
+            self.write('Load order tracking data')
+            return self.load_tracks()
 
         q = Queue()
 
@@ -147,20 +153,10 @@ class Command(DropifiedBaseCommand):
         self.create_property('dr_gkart_count', 'Groovekart Store Count', 'number', 'number')
         self.create_property('dr_bigcommerce_count', 'Bigcommerce Store Count', 'number', 'number')
 
-        self.create_property('dr_orders_7_day_count', 'Orders Count last 7 Days', 'number', 'number')
-        self.create_property('dr_orders_14_day_count', 'Orders Count last 14 Days', 'number', 'number')
         self.create_property('dr_orders_30_day_count', 'Orders Count last 30 Days', 'number', 'number')
-        self.create_property('dr_orders_90_day_count', 'Orders Count last 90 Days', 'number', 'number')
-        self.create_property('dr_orders_120_day_count', 'Orders Count last 120 Days', 'number', 'number')
-        self.create_property('dr_orders_365_day_count', 'Orders Count last 365 Days', 'number', 'number')
         self.create_property('dr_orders_all_count', 'Orders Count all time', 'number', 'number')
 
-        self.create_property('dr_orders_7_day_sum', 'Orders Sum last 7 Days', 'number', 'number')
-        self.create_property('dr_orders_14_day_sum', 'Orders Sum last 14 Days', 'number', 'number')
         self.create_property('dr_orders_30_day_sum', 'Orders Sum last 30 Days', 'number', 'number')
-        self.create_property('dr_orders_90_day_sum', 'Orders Sum last 90 Days', 'number', 'number')
-        self.create_property('dr_orders_120_day_sum', 'Orders Sum last 120 Days', 'number', 'number')
-        self.create_property('dr_orders_365_day_sum', 'Orders Sum last 365 Days', 'number', 'number')
         self.create_property('dr_orders_all_sum', 'Orders Sum All time', 'number', 'number')
 
         self.create_property('dr_mrr', 'Users MRR', 'number', 'number')
@@ -232,118 +228,19 @@ class Command(DropifiedBaseCommand):
         return api_requests(url, data, 'PATCH')
 
     def find_orders(self):
-        filename = 'query_result.csv'
-        orders_info = []
+        date_limit = arrow.utcnow().replace(days=-30).datetime
+        revenues = ShopifyOrderRevenue.objects.filter(created_at__gte=date_limit) \
+            .values('user_id').annotate(sum=Sum('total_price_usd')).order_by('-sum')
 
-        self.write('> Load CSV file')
-        with open(filename) as infile:
-            for line in infile:
-                store_id, created_at, total_sum, count = line.strip().split(',')
-                if not safe_int(store_id):
-                    continue
-
-                orders_info.append({
-                    'store_id': int(store_id),
-                    'created_at': arrow.get(created_at),
-                    'sum': safe_float(total_sum),
-                    'count': safe_int(count),
+        for revenue in revenues:
+            self.write(f"{revenue['user_id']} => {revenue['sum']}")
+            try:
+                user = User.objects.get(id=revenue['user_id'])
+                user.set_config('_shopify_orders_revenue', {
+                    '30': revenue['sum'],
                 })
-
-        orders_info = sorted(orders_info, key=lambda i: i['created_at'], reverse=True)
-
-        def get_default_dates(n):
-            return {
-                'days': n,
-                'week': int(n / 7) if n > 0 else n,
-                'orders': 0,
-                'sum': 0,
-                'count': 0,
-            }
-
-        now = arrow.utcnow()
-
-        self.write('> Calc orders info')
-        store_orders = {}
-        for order in orders_info:
-            sid = str(order['store_id'])
-            if sid not in store_orders:
-                store_orders[sid] = {}
-                for t in INTERVALS:
-                    store_orders[sid][t] = get_default_dates(int(t))
-
-            for interval in INTERVALS:
-                if interval == '-1' or \
-                    (now > now.replace(days=-store_orders[sid][interval]['days'])
-                        and store_orders[sid][interval]['orders'] < store_orders[sid][interval]['week']):
-                    store_orders[sid][interval]['orders'] += 1
-                    store_orders[sid][interval]['sum'] += order['sum']
-                    store_orders[sid][interval]['count'] += order['count']
-
-        self.write('> Stores map')
-        store_user_map = {}
-        store_currency_map = {}
-        for store in ShopifyStore.objects.all():
-            store_user_map[str(store.id)] = str(store.user_id)
-            currency = 1.0  # USD
-            currency_format = safe_str(store.currency_format).strip()
-            if '€' in currency_format or 'EUR' in currency_format:
-                currency = 1.14
-            elif '£' in currency_format:
-                currency = 1.34
-            elif 'Rs.' in currency_format:
-                currency = 0.013
-            elif 'R$' in currency_format:
-                currency = 0.18
-            elif '₫' in currency_format:
-                currency = 0.000044
-            elif 'R {{amount}}' == currency_format:
-                currency = 0.065
-            elif '₱' in currency_format:
-                currency = 0.020
-            elif 'Tk {{amount}}' in currency_format:
-                currency = 0.012
-            elif '¥' in currency_format:
-                currency = 0.0088
-            elif ' sk' in currency_format:
-                currency = 0.11
-            elif '{{amount}} SR' in currency_format:
-                currency = 0.27
-            elif 'SFr.' in currency_format:
-                currency = 1.09
-            elif ' dh' in currency_format or 'dhs' in currency_format:
-                currency = 0.11
-
-            store_currency_map[str(store.id)] = currency
-
-        self.write('> Merge users info')
-        user_orders_sum = defaultdict(dict)
-        user_orders_count = defaultdict(dict)
-
-        for key, val in store_orders.items():
-            if key not in store_user_map:
+            except User.DoesNotExist:
                 continue
-
-            sid = store_user_map[key]
-            for t in INTERVALS:
-                if t not in user_orders_sum[sid]:
-                    user_orders_sum[sid][t] = 0.0
-                user_orders_sum[sid][t] += val[t]['sum'] * store_currency_map[key]
-
-                if t not in user_orders_count[sid]:
-                    user_orders_count[sid][t] = 0
-                user_orders_count[sid][t] += val[t]['count']
-
-        self.write(f'user_orders_sum: {len(user_orders_sum)}, user_orders_count: {len(user_orders_count)}')
-
-        self.progress_total(User.objects.count())
-        for user in User.objects.all():
-            self.progress_update()
-            uid = str(user.id)
-            if uid in user_orders_sum or uid in user_orders_count:
-                user.set_config('_shopify_orders_stat', {
-                    'count': user_orders_count[uid],
-                    'sum': user_orders_sum[uid]
-                })
 
     def is_seen(self, user):
         try:
@@ -512,3 +409,36 @@ class Command(DropifiedBaseCommand):
                 yield row_value
             else:
                 first_row = False
+
+    def load_tracks(self):
+        self.write(f'https://api.keen.io/3.0/projects/{settings.KEEN_PROJECT_ID}/queries/count')
+        self.write(f'/{settings.KEEN_READ_KEY}/')
+
+        rep = requests.post(
+            url=f'https://api.keen.io/3.0/projects/{settings.KEEN_PROJECT_ID}/queries/count',
+            headers={'Authorization': settings.KEEN_READ_KEY},
+            data={
+                "analysis_type": "count",
+                "event_collection": "auto_fulfill",
+                "timezone": "UTC",
+                "group_by": ["user_id"],
+                "interval": None,
+                "timeframe": "this_30_days",
+                "zero_fill": None,
+                "filters": [],
+                "order_by": '[{"direction": "DESC", "property_name": "result"}]'
+            }
+        )
+
+        if rep.ok:
+            for result in rep.json()['result']:
+                self.write(f"{result['user_id']} => {result['result']}")
+                try:
+                    user = User.objects.get(id=result['user_id'])
+                    user.set_config('_shopify_orders_count', {
+                        '30': result['result'],
+                    })
+                except User.DoesNotExist:
+                    continue
+        else:
+            self.write(f'Keen Error: {rep.text}')
