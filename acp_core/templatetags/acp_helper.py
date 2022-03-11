e1109013f8328ab01c7f0022dc989c838777d6ed
Marian Gheata <victor960035@gmail.com>
2022-03-11 20:50:11 +0000

    Fix coloring in ACP

---
+++ acp_core/templatetags/acp_helper.py
@@ -0,0 +1,40 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.simple_tag
+def shopify_charge_class(charge):
+    status = charge.get('status')
+    if status == 'active':
+        cls = 'color-ok'
+    elif status == 'expired':
+        cls = 'color-warning'
+    else:
+        cls = 'color-danger'
+
+    return mark_safe(f' class="{cls}" ')
+
+
+@register.simple_tag
+def shopify_sub_class(charge):
+    return shopify_charge_class(charge)
+
+
+@register.simple_tag
+def stripe_charge_class(charge):
+    status = charge.get('status')
+    cls = None
+    if status == 'active' and not charge.dispute:
+        if charge.amount_refunded:
+            cls = 'color-warning'
+        else:
+            cls = 'color-ok'
+
+    elif status == 'succeeded':
+        cls = 'color-ok'
+    elif status == 'failed':
+        cls = 'color-danger'
+
+    return mark_safe(f' class="{cls}" ')
