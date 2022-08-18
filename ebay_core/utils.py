274886316810c01c5b943c9e3306014b36869810
Marian Gheata <victor960035@gmail.com>
2022-08-18 22:22:52 +0000

    Fix CI

--- ebay_core/utils.py
+++ ebay_core/utils.py
@@ -1893,9 +1893,9 @@ class EbayOrderItem:
 
 
 @add_to_class(UserProfile, 'sync_ebay_stores')
-def user_sync_ebay_stores(self):
+def user_sync_ebay_stores(self, use_cached=False):
     if self.user:
-        EbayUtils(self.user).sync_ebay_stores()
+        EbayUtils(self.user).sync_ebay_stores(use_cached=use_cached)
 
 
 def get_store_from_request(request):
